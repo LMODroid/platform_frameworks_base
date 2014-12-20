326f6020d76c8e75f302c13a5917afd9cad01726
nadlabak <pavel@doshaska.net>
2014-12-20 23:56:55 +0200

    Forward port 'Swap volume buttons' (1/3)
    [mikeioannina]: Adjust for 5.0 changes
    
    Change-Id: I1b45da63b815aa1b3ddf7cda2b7afb0872ab433f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5412,6 +5412,12 @@ public final class Settings {
         public static final String KEY_EDGE_LONG_SWIPE_ACTION = "key_edge_long_swipe_action";
 
         /**
+         * Swap volume buttons on rotation
+         * @hide
+         */
+        public static final String SWAP_VOLUME_KEYS_ON_ROTATION = "swap_volume_keys_on_rotation";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
