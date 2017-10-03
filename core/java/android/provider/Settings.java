f25a907df901d275e418018c6e68863c2ef0849e
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-03 17:17:37 -0400

    SystemUI: Add double tap to sleep gesture
    Author: Roman Birg <roman@cyngn.com>
    Date:   Sun Nov 23 06:54:06 2014 -0800
    
        SystemUI: double tap to sleep improvements
    
        * Make it more reliable
        * Add it to keyguard
        * Add a content observer to not always query Settings.System on every
        touch event
    
        Change-Id: I292c4d9d9f810843590b7a9ec6e15b99ac44009d
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Wed Nov 11 12:05:59 2015 -0800
    
        fw: Move DOUBLE_TAP_SLEEP_GESTURE to CMSettings.
    
        Change-Id: I8274b7c241cef6835a1114a702e68c95b6d2e036
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Oct 7 08:56:25 2016 +0800
    
        SystemUI: Use Tuner API for CM settings
    
        Get rid of all the excess code by implementing TunerService.Tunable
        and observing any changes made to the settings through it.
    
        Remove UserContentObserver as the Tuner API handles user switches.
    
        Also remove some unused imports that were introduced in earlier
        CM commits, but were never removed since.
    
        Change-Id: Iecafafabdaec82b3b3c72293bea865de48f0e90a
    
    Author: Altaf-Mahdi <altaf.mahdi@gmail.com>
    Date:   Wed Nov 11 16:07:49 2015 -0500
    
        Double tap to sleep anywhere on the lock screen [1/3]
    
        Change-Id: I7dd46f3fafeb2e629974c0f32083d4d9774fb1de
        [neo: Using Tuner API.]
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Author: dianlujitao <dianlujitao@lineageos.org>
    Date:   Thu Feb 27 12:57:07 2020 +0800
    
        SystemUI: Don't sleep on double tap below status bar
    
        Change-Id: Ic64c29eae63e96f34dc37cda355401b7e2cd2d39
    
    [Pig]: Forward port to R
    [bgcngm]: Forward port to S
    
    Change-Id: I0281d23cb3ad5f8f2b67d08ae431e8a962a60927

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10346,6 +10346,12 @@ public final class Settings {
                 "device_state_rotation_lock";
 
         /**
+         *  Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
