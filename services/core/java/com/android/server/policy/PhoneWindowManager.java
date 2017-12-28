d17cbfd96f70996c7e8a786cda3566fcbda879c9
Danesh Mondegarian <daneshm90@gmail.com>
2017-12-28 15:30:23 +0100

    PhoneWindowManager: Forward port long press back to kill app
    Change-Id: If3ed27e8408cdf383653c7d18988112c13f8bcea

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1674,6 +1674,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case SPLIT_SCREEN:
                 toggleSplitScreen();
                 break;
+            case KILL_APP:
+                ActionUtils.killForegroundApp(mContext, mCurrentUserId);
+                break;
             default:
                 break;
         }
