1bb71b2f9f8a4ad6076615e600a55c05ae447607
althafvly <althafvly@gmail.com>
2020-05-26 21:17:59 +0800

    SystemUI: support black theme for dark mode [1/4]
    Also includes squashed change:
    
    Author: Michael Bestas <mkbestas@gmail.com>
    Date:   Mon Sep 26 17:29:51 2022 +0300
    
        fixup! SystemUI: support black theme for dark mode [1/4]
    
        Fixes color picker
    
        Change-Id: If29713907a836384c024a4b66ea461acd97f2267
    
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Change-Id: I57cd53de8f2c1e4d445441b514875b6af915b858

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11613,6 +11613,12 @@ public final class Settings {
         public static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
         /**
+         * Whether to use black theme for dark mode
+         * @hide
+         */
+        public static final String BERRY_BLACK_THEME = "berry_black_theme";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
