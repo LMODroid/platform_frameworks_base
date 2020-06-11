3cf277dbe70194d13fbe753f300b57326ef2d435
utzcoz <utzcoz@outlook.com>
2020-06-11 20:16:55 +0800

    Add back button for freeform window
    Thanks to @RadekBledowski for the graphics
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>
    Change-Id: Ic847315ef5fb606cacde03f30ad0ec41730d36b7

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -1049,6 +1049,10 @@ public class Activity extends ContextThemeWrapper
             return ActivityClient.getInstance().moveActivityTaskToBack(mToken, nonRoot);
         }
 
+        @Override
+        public void onBackPressed() {
+            Activity.this.onBackPressed();
+        }
     };
 
     private static native String getDlWarning();
