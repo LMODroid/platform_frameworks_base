422a7bf047aba29520e18ae6985280256a949e9d
Arne Coucheron <arco68@gmail.com>
2021-03-30 01:41:16 +0200

    keyguard: Do not trigger a wake up when hiding lockscreen
    * This piece of code was introduced in commit:
          Swipe up to unlock when pulsing
          Change-Id: Ida5adb6a18f3ee992b1d154e10b35ba244f5b712
      However, it appears to not be needed and instead causes the device
      to wakeup right after dozing with no lockscreen set.
    
    * Remove it and test with:
      adb shell am broadcast -a com.android.systemui.doze.pulse com.android.systemui
      The device can still be unlocked from pulsing state.
    
    Change-Id: Ib5c2f930bd4de04cbf8f89f12d046f30c9b32a52
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -3006,10 +3006,10 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
 
             // It's possible that the device was unlocked (via BOUNCER or Fingerprint) while
             // dreaming. It's time to wake up.
-            if ((mDreamOverlayShowing || mUpdateMonitor.isDreaming()) && !mOrderUnlockAndWake) {
-                mPM.wakeUp(mSystemClock.uptimeMillis(), PowerManager.WAKE_REASON_GESTURE,
-                        "com.android.systemui:UNLOCK_DREAMING");
-            }
+            // if ((mDreamOverlayShowing || mUpdateMonitor.isDreaming()) && !mOrderUnlockAndWake) {
+            //     mPM.wakeUp(mSystemClock.uptimeMillis(), PowerManager.WAKE_REASON_GESTURE,
+            //             "com.android.systemui:UNLOCK_DREAMING");
+            // }
         }
         Trace.endSection();
     }
