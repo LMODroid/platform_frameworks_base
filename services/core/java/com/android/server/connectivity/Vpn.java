6f72fbc1841c54297c140f58f6239b168fc00954
Oliver Scott <olivercscott@gmail.com>
2021-07-08 10:41:43 -0400

    Global VPN feature [1/2]
    * Modify existing VPN user range functions to conditionally have traffic
      from all users pass through the global VPN.
      These functions are called when:
        1. Starting a VPN
        2. Adding a user
        3. Removing a user
    * Disallow starting VPNs in secondary users when a global VPN is set
    
    Also includes:
    Author: Oliver Scott <olivercscott@gmail.com>
    Date: 2021-08-27 16:30:22 -0400
    
        Show Global VPN icon on all users
    
        Change-Id: I496c0abbdf92b8f823bc57b297473aa14bd968c8
    
    Change-Id: I42616cc1f4e39e1dad739d81f6d5c55e218be995
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -153,6 +153,8 @@ import com.android.server.net.BaseNetworkObserver;
 import com.android.server.vcn.util.MtuUtils;
 import com.android.server.vcn.util.PersistableBundleUtils;
 
+import com.libremobileos.providers.LMOSettings;
+
 import libcore.io.IoUtils;
 
 import java.io.FileDescriptor;
@@ -772,6 +774,15 @@ public class Vpn {
      */
     public synchronized boolean getAlwaysOn() {
         return mAlwaysOn;
+    }
+
+    /**
+     * Returns whether currently prepared VPN package is set as the global VPN.
+     */
+    private synchronized boolean isGlobalVpn() {
+        final String globalVpnPkg = Settings.Global.getString(mContext.getContentResolver(),
+                LMOSettings.Global.GLOBAL_VPN_APP);
+        return mUserId == UserHandle.USER_SYSTEM && mPackage.equals(globalVpnPkg);
     }
 
     /**
@@ -1716,6 +1727,7 @@ public class Vpn {
         try {
             // Restricted users are not allowed to create VPNs, they are tied to Owner
             enforceNotRestrictedUser();
+            enforceNotGlobalVpn();
 
             final PackageManager packageManager = mUserIdContext.getPackageManager();
             if (packageManager == null) {
@@ -1866,7 +1878,7 @@ public class Vpn {
         addUserToRanges(ranges, userId, allowedApplications, disallowedApplications);
 
         // If the user can have restricted profiles, assign all its restricted profiles too
-        if (canHaveRestrictedProfile(userId)) {
+        if (canHaveRestrictedProfile(userId) || isGlobalVpn()) {
             final long token = Binder.clearCallingIdentity();
             List<UserInfo> users;
             try {
@@ -1875,7 +1887,8 @@ public class Vpn {
                 Binder.restoreCallingIdentity(token);
             }
             for (UserInfo user : users) {
-                if (user.isRestricted() && (user.restrictedProfileParentId == userId)) {
+                if ((user.isRestricted() && (user.restrictedProfileParentId == userId))
+                        || isGlobalVpn()) {
                     addUserToRanges(ranges, user.id, allowedApplications, disallowedApplications);
                 }
             }
@@ -1956,7 +1969,8 @@ public class Vpn {
     public void onUserAdded(int userId) {
         // If the user is restricted tie them to the parent user's VPN
         UserInfo user = mUserManager.getUserInfo(userId);
-        if (user.isRestricted() && user.restrictedProfileParentId == mUserId) {
+        if ((user.isRestricted() && user.restrictedProfileParentId == mUserId) ||
+                isGlobalVpn()) {
             synchronized(Vpn.this) {
                 final Set<Range<Integer>> existingRanges = mNetworkCapabilities.getUids();
                 if (existingRanges != null) {
@@ -1985,7 +1999,8 @@ public class Vpn {
     public void onUserRemoved(int userId) {
         // clean up if restricted
         UserInfo user = mUserManager.getUserInfo(userId);
-        if (user.isRestricted() && user.restrictedProfileParentId == mUserId) {
+        if ((user.isRestricted() && user.restrictedProfileParentId == mUserId) ||
+                isGlobalVpn()) {
             synchronized(Vpn.this) {
                 final Set<Range<Integer>> existingRanges = mNetworkCapabilities.getUids();
                 if (existingRanges != null) {
@@ -2410,6 +2425,17 @@ public class Vpn {
         }
     }
 
+    private void enforceNotGlobalVpn() {
+        Binder.withCleanCallingIdentity(() -> {
+            if (mUserId != UserHandle.USER_SYSTEM && !TextUtils.isEmpty(
+                    Settings.Global.getString(mContext.getContentResolver(),
+                            LMOSettings.Global.GLOBAL_VPN_APP))) {
+                throw new SecurityException("Secondary users cannot configure VPNs when" +
+                        " global vpn is set");
+            }
+        });
+    }
+
     /**
      * Start legacy VPN, controlling native daemons as needed. Creates a
      * secondary thread to perform connection work, returning quickly.
@@ -2493,6 +2519,7 @@ public class Vpn {
                     new UserHandle(mUserId))) {
             throw new SecurityException("Restricted users cannot establish VPNs");
         }
+        enforceNotGlobalVpn();
 
         // Load certificates.
         String privateKey = "";
@@ -4027,6 +4054,7 @@ public class Vpn {
 
         verifyCallingUidAndPackage(packageName);
         enforceNotRestrictedUser();
+        enforceNotGlobalVpn();
         validateRequiredFeatures(profile);
 
         if (profile.isRestrictedToTestNetworks) {
@@ -4069,6 +4097,7 @@ public class Vpn {
 
         verifyCallingUidAndPackage(packageName);
         enforceNotRestrictedUser();
+        enforceNotGlobalVpn();
 
         final long token = Binder.clearCallingIdentity();
         try {
@@ -4138,6 +4167,7 @@ public class Vpn {
         requireNonNull(packageName, "No package name provided");
 
         enforceNotRestrictedUser();
+        enforceNotGlobalVpn();
 
         // Prepare VPN for startup
         if (!prepare(packageName, null /* newPackage */, VpnManager.TYPE_VPN_PLATFORM)) {
@@ -4260,6 +4290,7 @@ public class Vpn {
         requireNonNull(packageName, "No package name provided");
 
         enforceNotRestrictedUser();
+        enforceNotGlobalVpn();
 
         // To stop the VPN profile, the caller must be the current prepared package and must be
         // running an Ikev2VpnProfile.
