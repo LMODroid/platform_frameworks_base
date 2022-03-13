a5a012dcdba7d8bdc3fa638dbc53651a4d1ac47b
nift4 <nift4@protonmail.com>
2022-03-13 08:04:10 +0100

    System UI tuner: Unlock 'em all!
    Now that everything is cleaned up
    
    Change-Id: I3ffb98cac7076d3b95db28b08366e75728e20f3e

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -40,9 +40,6 @@ public class TunerFragment extends PreferenceFragment {
 
     private static final String WARNING_TAG = "tuner_warning";
     private static final String[] DEBUG_ONLY = new String[] {
-            "nav_bar",
-            "lockscreen",
-            "picture_in_picture",
     };
 
     private final TunerService mTunerService;
