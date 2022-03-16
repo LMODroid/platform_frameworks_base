b997cc1799adee796017d9dfe0157b63ef9c07a0
nift4 <nift4@protonmail.com>
2022-03-16 19:14:47 +0100

    Revert "Remove "Device Controls" from Settings"
    This reverts commit 73702413f83353785ef109be1bd5c73befdcc148.
    
    Change-Id: Idc55a0515983652fff1f68cd6503f46c607591cd

--- packages/SystemUI/multivalentTests/src/com/android/systemui/qs/tiles/DeviceControlsTileTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/qs/tiles/DeviceControlsTileTest.kt
@@ -116,6 +116,7 @@ class DeviceControlsTileTest(flags: FlagsParameterization) : SysuiTestCase() {
         spiedContext = spy(mContext)
         doNothing().`when`(spiedContext).startActivity(any(Intent::class.java))
         `when`(qsHost.context).thenReturn(spiedContext)
+        `when`(controlsController.available).thenReturn(true)
         `when`(controlsComponent.isEnabled()).thenReturn(true)
         `when`(controlsController.getPreferredSelection())
             .thenReturn(
@@ -179,6 +180,14 @@ class DeviceControlsTileTest(flags: FlagsParameterization) : SysuiTestCase() {
         tile = createTile()
 
         assertThat(tile.isAvailable).isFalse()
+    }
+
+    @Test
+    fun testAvailableControlsSettingOff() {
+        `when`(controlsController.available).thenReturn(false)
+
+        tile = createTile()
+        assertThat(tile.isAvailable).isTrue()
     }
 
     @Test
