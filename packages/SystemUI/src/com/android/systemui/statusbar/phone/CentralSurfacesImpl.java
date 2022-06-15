44b73ea6d32d74438e3d4c237151cab97c736427
Pranav Vashi <neobuddy89@gmail.com>
2022-06-16 02:38:03 +0530

    ScrimController: Use ContentObserver to apply custom alpha
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I65ac6494b25c5d530f53d0b4e957de0db01ed9a6
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -53,6 +53,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Configuration;
+import android.database.ContentObserver;
 import android.graphics.Point;
 import android.hardware.devicestate.DeviceStateManager;
 import android.metrics.LogMaker;
@@ -240,6 +241,8 @@ import com.android.systemui.wallet.controller.QuickAccessWalletController;
 import com.android.wm.shell.bubbles.Bubbles;
 import com.android.wm.shell.startingsurface.SplashscreenContentDrawer;
 import com.android.wm.shell.startingsurface.StartingSurface;
+
+import com.libremobileos.providers.LMOSettings;
 
 import dalvik.annotation.optimization.NeverCompile;
 
@@ -888,6 +891,24 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         mKeyguardIndicationController.init();
 
         mColorExtractor.addOnColorsChangedListener(mOnColorsChangedListener);
+
+        Uri qsTransparency = Settings.System.getUriFor(LMOSettings.System.QS_TRANSPARENCY);
+        ContentObserver contentObserver = new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange, Uri uri) {
+                if (uri.equals(qsTransparency)) {
+                    int newValue = Settings.System.getIntForUser(mContext.getContentResolver(),
+                            LMOSettings.System.QS_TRANSPARENCY, 100,
+                            UserHandle.USER_CURRENT);
+                    mContext.getMainExecutor().execute(() -> {
+                        mScrimController.setCustomScrimAlpha(newValue);
+                    });
+                }
+            }
+        };
+        mContext.getContentResolver().registerContentObserver(
+                qsTransparency, false, contentObserver);
+        contentObserver.onChange(true, qsTransparency);
 
         mDisplay = mContext.getDisplay();
         mDisplayId = mDisplay.getDisplayId();
