07b74a2d16b5b490a633d72e4705bed2bafd5b90
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-06-16 22:32:56 +0430

    SystemUI: Add VPNTethering tile
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I2d8924bc6341e0dafa2a4db69c25e6200bb5c288

--- packages/SystemUI/src/com/android/systemui/libremobileos/LMODroidModule.kt
+++ packages/SystemUI/src/com/android/systemui/libremobileos/LMODroidModule.kt
@@ -26,6 +26,8 @@ import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
+import com.android.systemui.qs.tiles.VPNTetheringTile
+
 import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.WifiTile
 
@@ -89,6 +91,12 @@ interface LMODroidModule {
     @IntoMap
     @StringKey(UsbTetherTile.TILE_SPEC)
     fun bindUsbTetherTile(usbTetherTile: UsbTetherTile): QSTileImpl<*>
+
+    /** Inject VPNTetheringTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(VPNTetheringTile.TILE_SPEC)
+    fun bindVPNTetheringTile(vpnTetheringTile: VPNTetheringTile): QSTileImpl<*>
 
     /** Inject VpnTile into tileMap in QSModule */
     @Binds
