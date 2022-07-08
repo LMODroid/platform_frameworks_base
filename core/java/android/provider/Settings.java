3c83016e850fc4e79c9b22c0780d7dc5df6972f7
ReallySnow <reallysnow233@gmail.com>
2022-07-08 17:36:35 +0800

    fw/b: Import Wi-Fi standard icon [1/2]
    * Show Wi-Fi 4/5/6 standard on statusbar
    * Updated icons from: https://gerrit.aospa.co/c/AOSPA/android_frameworks_base/+/23515
    
    Co-authored-by: Col_or <col_or@qq.com>
    Co-authored-by: Kujou Yuko <i@779.moe>
    Change-Id: I3658cda5b303bd03cf83e174e4dfd6a55d33e860

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11134,6 +11134,12 @@ public final class Settings {
         public static final String POWER_MENU_ACTIONS = "power_menu_actions";
 
         /**
+         * Whether to show Wi-Fi standard icon
+         * @hide
+         */
+        public static final String SHOW_WIFI_STANDARD_ICON = "show_wifi_standard_icon";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
