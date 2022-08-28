36fa0895f9ee35e01478a401f4c6a56137b1d94b
nift4 <nift4@protonmail.com>
2022-08-28 14:02:08 +0200

    InputManager: allow to force NULL cursor
    If VNC draws cursor on client side, we do not want to have a second
    cursor visible.
    
    Change-Id: Iab1b9477a9142f7ae23484f76285981bddca1880

--- services/core/java/com/android/server/input/InputManagerService.java
+++ services/core/java/com/android/server/input/InputManagerService.java
@@ -259,6 +259,7 @@ public class InputManagerService extends IInputManager.Stub
     private final SparseArray<CursorCallbackRecord> mCursorCallbacks = new SparseArray<>();
     private final List<CursorCallbackRecord> mTempCursorCallbacksToNotify =
             new ArrayList<>();
+    private boolean mForceNullCursor = false;
 
     // State for the currently installed input filter.
     final Object mInputFilterLock = new Object();
@@ -550,6 +551,15 @@ public class InputManagerService extends IInputManager.Stub
         synchronized (mLidSwitchLock) {
             mLidSwitchCallbacks.remove(callback);
         }
+    }
+
+    @Override // Binder call
+    public void setForceNullCursor(boolean forceNullCursor) {
+        if (DEBUG) {
+            Slog.d(TAG, "setForceNullCursor: forceNullCursor=" + forceNullCursor + " callingPid="
+                    + Binder.getCallingPid());
+        }
+        mForceNullCursor = forceNullCursor;
     }
 
     @Override // Binder call
@@ -1724,6 +1734,9 @@ public class InputManagerService extends IInputManager.Stub
             IBinder inputToken) {
         Objects.requireNonNull(icon);
         deliverCursorChanged(icon.getType(), icon);
+        if (mForceNullCursor) {
+            icon = PointerIcon.getSystemIcon(mContext, PointerIcon.TYPE_NULL);
+        }
         return mNative.setPointerIcon(icon, displayId, deviceId, pointerId, inputToken);
     }
 
