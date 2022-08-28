01f5537902961e0087d50bbdc9c5ae688b1dd3b4
Genkzsz11 <genkzsz11@gmail.com>
2022-08-28 15:57:28 +0700

    SystemUI: Update gamespace for A13
    Change-Id: I70357f647f6cb8f719f1523e4dbaf3c0207edfb5
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -4447,6 +4447,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
         return mGutsManager;
     }
 
+    @Override
     public GameSpaceManager getGameSpaceManager() {
         return mGameSpaceManager;
     }
