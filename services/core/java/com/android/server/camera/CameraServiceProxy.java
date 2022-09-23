57320514c0ca8f52c529f3434ba9d3df7cf298cb
LibXZR <i@xzr.moe>
2022-09-23 14:56:13 +0800

    services: camera: Whitelist parallel space users
    Fixes unable to use camera in parallel spaces.
    
    Change-Id: I42f412cfb3a803182fc98d10c2a496c5aa3116bc
    Signed-off-by: LibXZR <i@xzr.moe>

--- services/core/java/com/android/server/camera/CameraServiceProxy.java
+++ services/core/java/com/android/server/camera/CameraServiceProxy.java
@@ -76,6 +76,7 @@ import com.android.server.LocalServices;
 import com.android.server.ServiceThread;
 import com.android.server.SystemService;
 import com.android.server.wm.WindowManagerInternal;
+import com.android.server.libremobileos.ParallelSpaceManagerService;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -329,6 +330,7 @@ public class CameraServiceProxy extends SystemService
                 case Intent.ACTION_USER_INFO_CHANGED:
                 case Intent.ACTION_MANAGED_PROFILE_ADDED:
                 case Intent.ACTION_MANAGED_PROFILE_REMOVED:
+                case Intent.ACTION_PARALLEL_SPACE_CHANGED:
                     synchronized(mLock) {
                         // Return immediately if we haven't seen any users start yet
                         if (mEnabledCameraUsers == null) return;
@@ -645,6 +647,7 @@ public class CameraServiceProxy extends SystemService
         filter.addAction(Intent.ACTION_USER_INFO_CHANGED);
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_ADDED);
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_REMOVED);
+        filter.addAction(Intent.ACTION_PARALLEL_SPACE_CHANGED);
         mContext.registerReceiver(mIntentReceiver, filter);
 
         publishBinderService(CAMERA_SERVICE_PROXY_BINDER_NAME, mCameraServiceProxy);
@@ -880,6 +883,7 @@ public class CameraServiceProxy extends SystemService
         for (int id : userProfiles) {
             handles.add(id);
         }
+        handles.addAll(ParallelSpaceManagerService.getCurrentParallelUserIds());
 
         return handles;
     }
