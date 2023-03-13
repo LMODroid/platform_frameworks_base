b4d00590f59bb864031ca458a9560b9a51d3c8e8
Michael Bestas <mkbestas@gmail.com>
2023-03-14 00:21:13 +0200

    Revert "Keyguard: Allow user configurable fingerprint wake-and-unlock"
    Replaced by AOSP implementation
    
    This reverts commit 9a7dc3fa0b1319e46c69501b4383cf6e4ded0440.
    
    Change-Id: I2dba8ff3fe167eaea472f54b44498eba50330c65
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -156,8 +156,6 @@ import com.android.systemui.util.Assert;
 
 import com.google.android.collect.Lists;
 
-import android.provider.Settings;
-
 import java.io.PrintWriter;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -344,7 +342,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
             mCallbacks = Lists.newArrayList();
     private ContentObserver mDeviceProvisionedObserver;
     private ContentObserver mTimeFormatChangeObserver;
-    private ContentObserver mSettingsChangeObserver;
 
     private boolean mSwitchingUser;
 
@@ -373,7 +370,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
     private final PowerManager mPowerManager;
     private final boolean mWakeOnFingerprintAcquiredStart;
 
-    private boolean mFingerprintWakeAndUnlock;
+    private final boolean mFingerprintWakeAndUnlock;
 
     /**
      * Short delay before restarting fingerprint authentication after a successful try. This should
@@ -1950,6 +1947,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         mTelephonyListenerManager = telephonyListenerManager;
         mDeviceProvisioned = isDeviceProvisionedInSettingsDb();
         mStrongAuthTracker = new StrongAuthTracker(context, this::notifyStrongAuthStateChanged);
+        mFingerprintWakeAndUnlock = mContext.getResources().getBoolean(
+                com.android.systemui.R.bool.config_fingerprintWakeAndUnlock);
         mBackgroundExecutor = backgroundExecutor;
         mBroadcastDispatcher = broadcastDispatcher;
         mInteractionJankMonitor = interactionJankMonitor;
@@ -1974,8 +1973,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                         R.array.config_face_acquire_device_entry_ignorelist))
                 .boxed()
                 .collect(Collectors.toSet());
-
-        updateFingerprintSettings();
 
         mHandler = new Handler(mainLooper) {
             @Override
@@ -2233,32 +2230,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.TIME_12_24),
                 false, mTimeFormatChangeObserver, UserHandle.USER_ALL);
-
-        mSettingsChangeObserver = new ContentObserver(mHandler) {
-            @Override
-            public void onChange(boolean selfChange) {
-                updateFingerprintSettings();
-            }
-        };
-        mContext.getContentResolver().registerContentObserver(
-                Settings.System.getUriFor(Settings.System.FINGERPRINT_WAKE_UNLOCK),
-                false, mSettingsChangeObserver, UserHandle.USER_ALL);
-    }
-
-    private void updateFingerprintSettings() {
-        boolean defFingerprintSettings = mContext.getResources().getBoolean(
-                com.android.systemui.R.bool.config_fingerprintWakeAndUnlock);
-        if (defFingerprintSettings) {
-            mFingerprintWakeAndUnlock = Settings.System.getIntForUser(
-                    mContext.getContentResolver(), Settings.System.FINGERPRINT_WAKE_UNLOCK,
-                    1, UserHandle.USER_CURRENT) == 1;
-        } else {
-            mFingerprintWakeAndUnlock = defFingerprintSettings;
-            // if its false, the device meant to be used like that, disable toggle with 2.
-            Settings.System.putIntForUser(mContext.getContentResolver(),
-                    Settings.System.FINGERPRINT_WAKE_UNLOCK,
-                    2, UserHandle.USER_CURRENT);
-        }
     }
 
     private void updateFaceEnrolled(int userId) {
@@ -3775,10 +3746,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
         if (mTimeFormatChangeObserver != null) {
             mContext.getContentResolver().unregisterContentObserver(mTimeFormatChangeObserver);
-        }
-
-        if (mSettingsChangeObserver != null) {
-            mContext.getContentResolver().unregisterContentObserver(mSettingsChangeObserver);
         }
 
         try {
