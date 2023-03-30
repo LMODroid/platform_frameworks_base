3439de528a4b57f1ffe5706eebc0e418b3ffcd5e
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2023-03-30 09:18:21 +0330

    Revert "NetworkTraffic: Fix white on white text for Quick Statusbar"
    This reverts commit 4f0bb2e3977b642294f18a987336a91325438ba8.
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I762186597813b8cb92b8b210b9b4160c777f47b6

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -304,7 +304,6 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
             }
             mBatteryRemainingIcon.updateColors(mTextColorPrimary, textColorSecondary,
                     mTextColorPrimary);
-            mNetworkTraffic.setTint(textColor);
         }
 
         MarginLayoutParams qqsLP = (MarginLayoutParams) mHeaderQsPanel.getLayoutParams();
