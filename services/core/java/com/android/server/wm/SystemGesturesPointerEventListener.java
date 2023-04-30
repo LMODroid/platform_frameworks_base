7430290cf91d87fee81dd359d6361e94fdae92f0
nift4 <nift4@protonmail.com>
2023-04-30 11:03:47 +0200

    wm: QC-like gesture fling boost
    Idea by minaripenguin37 <alexfinhart@gmail.com> and hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
    
    Change-Id: I53b27395a9ab1e067f65011c0aabd9f3b186d324

--- services/core/java/com/android/server/wm/SystemGesturesPointerEventListener.java
+++ services/core/java/com/android/server/wm/SystemGesturesPointerEventListener.java
@@ -85,6 +85,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
     int screenWidth;
     private int mDownPointers;
     private boolean mSwipeFireable;
+    private boolean mScrollFireable;
     private boolean mDebugFireable;
     private boolean mMouseHoveringAtLeft;
     private boolean mMouseHoveringAtTop;
@@ -184,6 +185,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
             case MotionEvent.ACTION_DOWN:
                 mSwipeFireable = true;
                 mDebugFireable = true;
+                mScrollFireable = true;
                 mDownPointers = 0;
                 captureDown(event, 0);
                 if (mMouseHoveringAtLeft) {
@@ -290,6 +292,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
             case MotionEvent.ACTION_CANCEL:
                 mSwipeFireable = false;
                 mDebugFireable = false;
+                mScrollFireable = false;
                 mCallbacks.onUpOrCancel();
                 break;
             default:
@@ -431,6 +434,21 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
             }
             return true;
         }
+
+        @Override
+        public boolean onScroll(MotionEvent down, MotionEvent up,
+                                   float velocityX, float velocityY) {
+            int duration = mOverscroller.getDuration();
+            if (duration > MAX_FLING_TIME_MILLIS) {
+                duration = MAX_FLING_TIME_MILLIS;
+            }
+            if (mScrollFireable) {
+                mCallbacks.onFling(duration + 160);
+                mScrollFireable = false;
+            }
+            return true;
+        }
+
         @Override
         public boolean onFling(MotionEvent down, MotionEvent up,
                 float velocityX, float velocityY) {
@@ -447,7 +465,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
                 duration = MAX_FLING_TIME_MILLIS;
             }
             mLastFlingTime = now;
-            mCallbacks.onFling(duration);
+            mCallbacks.onFling(duration + 160);
             return true;
         }
     }
