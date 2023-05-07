b5014c20153c91b6600bd77a63cd7aed95476a38
nift4 <nift4@protonmail.com>
2023-05-07 12:52:54 +0200

    Force mouse event as touch [1/2]
    Change-Id: I51ad0e3894e04dc49c572421ca00ba321d3ace15

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6233,6 +6233,11 @@ public final class Settings {
         public static final String PREVENT_POINTER_ACCELERATION = "prevent_pointer_acceleration";
 
         /**
+         * @hide
+         */
+        public static final String FORCE_MOUSE_AS_TOUCH = "force_mouse_as_touch";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6359,6 +6364,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(POCKET_JUDGE);
             PRIVATE_SETTINGS.add(TRANSISTENT_TASK_MODE);
             PRIVATE_SETTINGS.add(PREVENT_POINTER_ACCELERATION);
+            PRIVATE_SETTINGS.add(FORCE_MOUSE_AS_TOUCH);
         }
 
         /**
@@ -6374,6 +6380,7 @@ public final class Settings {
             CLONE_TO_MANAGED_PROFILE.add(TEXT_SHOW_PASSWORD);
             CLONE_TO_MANAGED_PROFILE.add(TIME_12_24);
             CLONE_TO_MANAGED_PROFILE.add(TRANSISTENT_TASK_MODE);
+            CLONE_TO_MANAGED_PROFILE.add(FORCE_MOUSE_AS_TOUCH);
         }
 
         /** @hide */
--- services/core/java/com/android/server/input/InputManagerService.java
+++ services/core/java/com/android/server/input/InputManagerService.java
@@ -576,6 +576,7 @@ public class InputManagerService extends IInputManager.Stub
 
         registerPointerSpeedSettingObserver();
         registerPreventPointerAccelerationSettingObserver();
+        registerForceMouseAsTouchSettingObserver();
         registerShowTouchesSettingObserver();
         registerAccessibilityLargePointerSettingObserver();
         registerLongPressTimeoutObserver();
@@ -588,6 +589,7 @@ public class InputManagerService extends IInputManager.Stub
             public void onReceive(Context context, Intent intent) {
                 updatePointerSpeedFromSettings();
                 updatePreventPointerAccelerationFromSettings();
+                updateForceMouseAsTouchFromSettings();
                 updateShowTouchesFromSettings();
                 updateAccessibilityLargePointerFromSettings();
                 updateDeepPressStatusFromSettings("user switched");
@@ -597,6 +599,7 @@ public class InputManagerService extends IInputManager.Stub
 
         updatePointerSpeedFromSettings();
         updatePreventPointerAccelerationFromSettings();
+        updateForceMouseAsTouchFromSettings();
         updateShowTouchesFromSettings();
         updateAccessibilityLargePointerFromSettings();
         updateDeepPressStatusFromSettings("just booted");
@@ -1941,6 +1944,15 @@ public class InputManagerService extends IInputManager.Stub
         mNative.setPreventPointerAcceleration(preventPointerAcceleration);
     }
 
+    private void updateForceMouseAsTouchFromSettings() {
+        boolean forceMouseAsTouch = getForceMouseAsTouchSetting();
+        setForceMouseAsTouchUnchecked(forceMouseAsTouch);
+    }
+
+    private void setForceMouseAsTouchUnchecked(boolean forceMouseAsTouch) {
+        mNative.setForceMouseAsTouch(forceMouseAsTouch);
+    }
+
     private void setPointerAcceleration(float acceleration, int displayId) {
         updateAdditionalDisplayInputProperties(displayId,
                 properties -> properties.pointerAcceleration = acceleration);
@@ -1991,6 +2003,27 @@ public class InputManagerService extends IInputManager.Stub
         } catch (SettingNotFoundException ignored) {
         }
         return preventPointerAcceleration;
+    }
+
+    private void registerForceMouseAsTouchSettingObserver() {
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.FORCE_MOUSE_AS_TOUCH), true,
+                new ContentObserver(mHandler) {
+                    @Override
+                    public void onChange(boolean selfChange) {
+                        updateForceMouseAsTouchFromSettings();
+                    }
+                }, UserHandle.USER_ALL);
+    }
+
+    private boolean getForceMouseAsTouchSetting() {
+        boolean forceMouseAsTouch = false;
+        try {
+            forceMouseAsTouch = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.FORCE_MOUSE_AS_TOUCH, UserHandle.USER_CURRENT) > 0;
+        } catch (SettingNotFoundException ignored) {
+        }
+        return forceMouseAsTouch;
     }
 
     private void updateShowTouchesFromSettings() {
