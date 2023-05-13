66f2b03f5b17715d2620e83e869d2c5f7f890682
nift4 <nift4@protonmail.com>
2023-05-13 14:07:03 +0200

    SystemUI: fix transparent navigation bar keys in secondary display
    Huge thanks to @luka177 for investigation
    
    What happens here is simple: mContext has Theme.SystemUI set, for
    secondary displays, it uses createDisplayContext() which unsets Theme
    because it's not a UI context. If we manually set theme again, resources
    start resolving again and navigation bar keys are no longer transparent.
    
    Change-Id: Ib8c9212ac0facda8e0f4eeac2d4f839748178cc9

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarController.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarController.java
@@ -34,6 +34,7 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.ContextThemeWrapper;
 import android.view.Display;
 import android.view.IWindowManager;
 import android.view.View;
@@ -45,6 +46,7 @@ import androidx.annotation.Nullable;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.statusbar.RegisterStatusBarResult;
 import com.android.settingslib.applications.InterestingConfigChanges;
+import com.android.systemui.R;
 import com.android.systemui.Dumpable;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Main;
@@ -359,9 +361,9 @@ public class NavigationBarController implements
 
         final int displayId = display.getDisplayId();
         final boolean isOnDefaultDisplay = displayId == mDisplayTracker.getDefaultDisplayId();
-        final Context context = isOnDefaultDisplay
-                ? mContext
-                : mContext.createDisplayContext(display);
+
+        final Context rawContext = mContext.createDisplayContext(display);
+        final Context context = new ContextThemeWrapper(rawContext, R.style.Theme_SystemUI);
 
         if (!shouldCreateNavBarAndTaskBar(context, displayId)) {
             return;
