LMODroid/platform_frameworks_base

commit 3c2bfc8804d9fa268622233f22dee6ab88d290cb

CP: Don't create panel if content is empty
This CL fixes the issue where WM CTS is causing ANR in ARC.

When a menu panel is opened, the sub window for this panel is
created, but as it doesn't have any content, it's considered hidden
by SF. The problem here is that as focus has already moved to this
app at this point, InputDispatcher expects the window will get a
content soon, but this never happens and causes ANR.

So, the root problem here is that PhoneWindow creates a window with
WRAP_CONTENT even if there's no content set to the window. This
seems to be a pure bug in PhoneWindow because there's already some
logic where if it doesn't have any menu item, it aborts opening a
panel, but if this function is called twice, this return logic is
bypassed and the window is accidentally created (because whether
it goes thought the return logic depends on whether st.decorView
has an instance, and this is instantiated in the first call of
openPanel())

This CL ensures the sub window isn't created if there's no item to
be shown.

Bug: 180566381
Bug: 181543065
Test: atest CtsWindowManagerDeviceTestCases
Change-Id: I1a1ae84abf4e4466ebb4a6655a5be007ee763d78
(cherry picked from commit 45ae74eab837ccbd67534512688f4685442cf95a)
Merged-In: I1a1ae84abf4e4466ebb4a6655a5be007ee763d78

core/java/com/android/internal/policy/PhoneWindow.java
@@ -912,6 +912,12 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
             }
         }
 
+        if (!st.hasPanelItems()) {
+            // Ensure that |st.decorView| has its actual content. Otherwise, an empty window can be
+            // created and cause ANR.
+            return;
+        }
+
         st.isHandled = false;
 
         WindowManager.LayoutParams lp = new WindowManager.LayoutParams(

commit 8adcae48d36cb0f7015cfb3e19d1fcf3e6f11463

wm: Fix menu in legacy apps with multiple activities
To reproduce:
1. Open pre-honeycomb app in activity without options menu
2. adb shell input keyevent MENU
3. Observe menu not opening (correct behaviour)
4. Open activity with options menu in same app (without restarting app)
4. adb shell input keyevent MENU
5. Observe nothing happening (wrong behaviour)
6. adb shell input keyevent MENU
7. Observe menu opening and closing itself instantly (wrong behaviour)

Root cause:
When skipping creating empty panel, we do not update state to closed, so
app and window will disagree about menu open or not, which will cause
conflict between open and closed state.

Fixes: 3c2bfc88 ("CP: Don't create panel if content is empty")
Test: Reproduce steps now successfully open menu every time in activity
      with options menu.
Change-Id: I0501a2ffcccd30b0085dea7e2105ad57869d00da

core/java/com/android/internal/policy/PhoneWindow.java
@@ -871,6 +871,7 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
 
             // This will populate st.shownPanelView
             if (!initializePanelContent(st) || !st.hasPanelItems()) {
+                closePanel(st, true);
                 return;
             }
 
@@ -920,6 +921,7 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
         if ((st.shownPanelView == null && !initializePanelContent(st)) || !st.hasPanelItems()) {
             // Ensure that |st.decorView| has its actual content. Otherwise, an empty window can be
             // created and cause ANR.
+            closePanel(st, true);
             return;
         }
 