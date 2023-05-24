ee6995a6c13582c9de95b1c6c6e012e29c541ddc
aswin7469 <aswinas@pixysos.com>
2023-05-24 16:55:01 +0530

    base: Extend showing Ambient instead of fully waking to single tap gesture [1/2]
    [electimon: Modify for defaults]
    Change-Id: I5e5bf835a1e8ecec59dbc1ee0563a6adf30ad6e9
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -452,5 +452,6 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.MANDATORY_BIOMETRICS_REQUIREMENTS_SATISFIED,
                 new InclusiveIntegerRangeValidator(0, 1));
         VALIDATORS.put(Secure.ADVANCED_PROTECTION_MODE, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.DOZE_SINGLE_TAP_GESTURE_AMBIENT, BOOLEAN_VALIDATOR);
     }
 }
