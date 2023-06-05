07e7a5e24936b08005a9305b4ec4e319f95579a1
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2023-06-05 13:28:41 +0330

    fwb: Set default value of qs transparency as overlay [1/2]
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I55980bce6387e89df3611f11140ac886b3c0d495
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -60,6 +60,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
 import android.content.res.Configuration;
 import android.database.ContentObserver;
 import android.graphics.Point;
@@ -995,8 +996,9 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                         });
                     }
                 } else if (uri.equals(qsTransparency)) {
+                    final Resources res = mContext.getResources();
                     int newValue = Settings.System.getIntForUser(mContext.getContentResolver(),
-                            LMOSettings.System.QS_TRANSPARENCY, 100,
+                            LMOSettings.System.QS_TRANSPARENCY, res.getInteger(com.android.internal.R.integer.config_qs_transparency),
                             UserHandle.USER_CURRENT);
                     mContext.getMainExecutor().execute(() -> {
                         mScrimController.setCustomScrimAlpha(newValue);
