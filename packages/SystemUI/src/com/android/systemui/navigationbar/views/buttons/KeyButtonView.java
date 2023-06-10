a052b5e9057065c8ad80df842e7d82d0d5931bba
nift4 <nift4@protonmail.com>
2023-06-10 02:17:47 +0200

    SystemUI: tuner: navbar tuner refresh
    * Expose all AOSP keys to UI
    * Load proper default for 2button/gesture nav
    * Reset tuner key on nav mode change (support gesture nav)
    * Bring back custom keycode, bug fixes
    * Support dynamic height change (gesture nav extra buttons)
    * Support menu button on gesture nav by default
    * Support cursor key on gesture if IME nav rendering is off
    * Do NOT load custom layout if IME nav rendering is on + gesture on
    
    Change-Id: I1332e0e9d7f49cbc5c12c47e566b3c849c6c250b
    Signed-off-by: Farzin <itisFarzin@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/views/buttons/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/views/buttons/KeyButtonView.java
@@ -29,12 +29,10 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.Icon;
 import android.hardware.input.InputManager;
 import android.hardware.input.InputManagerGlobal;
 import android.media.AudioManager;
 import android.metrics.LogMaker;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.AttributeSet;
@@ -216,20 +214,6 @@ public class KeyButtonView extends ImageView implements ButtonInterface, DragDro
         mNavBarButtonClickLogger = navBarButtonClickLogger;
     }
 
-    public void loadAsync(Icon icon) {
-        new AsyncTask<Icon, Void, Drawable>() {
-            @Override
-            protected Drawable doInBackground(Icon... params) {
-                return params[0].loadDrawable(mContext);
-            }
-
-            @Override
-            protected void onPostExecute(Drawable drawable) {
-                setImageDrawable(drawable);
-            }
-        }.execute(icon);
-    }
-
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
@@ -393,8 +377,7 @@ public class KeyButtonView extends ImageView implements ButtonInterface, DragDro
         if (mHasOvalBg) {
             mOvalBgPaint.setColor(keyButtonDrawable.getDrawableBackgroundColor());
         }
-        mRipple.setType(keyButtonDrawable.hasOvalBg() ? KeyButtonRipple.Type.OVAL
-                : KeyButtonRipple.Type.ROUNDED_RECT);
+        mRipple.setType(mHasOvalBg ? KeyButtonRipple.Type.OVAL : KeyButtonRipple.Type.ROUNDED_RECT);
     }
 
     public void playSoundEffect(int soundConstant) {
