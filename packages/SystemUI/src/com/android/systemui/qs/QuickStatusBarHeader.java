0399896b7eab44df5d375534596dc5bde7d3be18
Michael Bestas <mkbestas@gmail.com>
2023-06-13 23:49:14 +0300

    Revert "SystemUI: Align QS header with status bar"
    Reason for revert: Conflicts with QPR3
    
    This reverts commit 8658042a3c1c662f6a14ea3bf311e1e6b07f785e.
    
    Change-Id: Id45819f4707a67c0719007d223fbfadd3b2c2ec3
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -110,7 +110,6 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
     private StatusBarContentInsetsProvider mInsetsProvider;
 
     private int mRoundedCornerPadding = 0;
-    private int mStatusBarPaddingTop;
     private int mWaterfallTopInset;
     private int mCutOutPaddingLeft;
     private int mCutOutPaddingRight;
@@ -288,20 +287,19 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         mRoundedCornerPadding = resources.getDimensionPixelSize(
                 R.dimen.rounded_corner_content_padding);
 
-        int statusBarHeight = SystemBarUtils.getStatusBarHeight(mContext);
+        int qsOffsetHeight = SystemBarUtils.getQuickQsOffsetHeight(mContext);
 
-        mStatusBarPaddingTop = resources.getDimensionPixelSize(
-                R.dimen.status_bar_padding_top);
-
-        mDatePrivacyView.getLayoutParams().height = statusBarHeight;
+        mDatePrivacyView.getLayoutParams().height =
+                Math.max(qsOffsetHeight, mDatePrivacyView.getMinimumHeight());
         mDatePrivacyView.setLayoutParams(mDatePrivacyView.getLayoutParams());
 
-        mStatusIconsView.getLayoutParams().height = statusBarHeight;
+        mStatusIconsView.getLayoutParams().height =
+                Math.max(qsOffsetHeight, mStatusIconsView.getMinimumHeight());
         mStatusIconsView.setLayoutParams(mStatusIconsView.getLayoutParams());
 
         ViewGroup.LayoutParams lp = getLayoutParams();
         if (mQsDisabled) {
-            lp.height = mStatusIconsView.getLayoutParams().height - mWaterfallTopInset;
+            lp.height = mStatusIconsView.getLayoutParams().height;
         } else {
             lp.height = WRAP_CONTENT;
         }
@@ -330,7 +328,7 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
             qqsLP.topMargin = mContext.getResources()
                     .getDimensionPixelSize(R.dimen.qqs_layout_margin_top);
         } else if (!mUseCombinedQSHeader) {
-            qqsLP.topMargin = SystemBarUtils.getQuickQsOffsetHeight(mContext);
+            qqsLP.topMargin = qsOffsetHeight;
         } else {
             qqsLP.topMargin = mContext.getResources()
                     .getDimensionPixelSize(R.dimen.large_screen_shade_header_min_height);
@@ -582,11 +580,11 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         }
 
         mDatePrivacyView.setPadding(paddingLeft,
-                mStatusBarPaddingTop,
+                mWaterfallTopInset,
                 paddingRight,
                 0);
         mStatusIconsView.setPadding(paddingLeft,
-                mStatusBarPaddingTop,
+                mWaterfallTopInset,
                 paddingRight,
                 0);
     }
