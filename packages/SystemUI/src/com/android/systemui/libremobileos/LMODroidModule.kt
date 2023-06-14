3bea0b337f06fa46bf1670de0119d247b2ca4a59
Michael Bestas <mkbestas@gmail.com>
2023-06-14 11:53:23 +0300

    [SQUASH] SystemUI: Restore wifi and cellular QS tiles
    Squashed:
        Commit: d11f82ff7349086229f39bb2400a23fdcea9bf0d
        From: Joe Maples <joe@maples.dev>
        QS: Add and use Mobile Data panel
    
        Change-Id: I9f818eea1ab906bb1721e0aab4cbb138187e507b
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
        Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    
        Commit: bb53ffab41889d41823eb80f299741cf7cee938f
        From: sb6596 <shubhamprince111@gmail.com>
        SystemUI: Open WifiPanel on LongClick
    
        Change-Id: Iebac79f1d2fde8dc810339ddd61fd39cde8db6c2
        Signed-off-by: sb6596 <shubhamprince111@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
        Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: Idda5bb5ba63f9d5fc39d27a9c663538ff321d224

--- packages/SystemUI/src/com/android/systemui/libremobileos/LMODroidModule.kt
+++ packages/SystemUI/src/com/android/systemui/libremobileos/LMODroidModule.kt
@@ -21,11 +21,13 @@ import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VPNTetheringTile
+import com.android.systemui.qs.tiles.WifiTile
 
 import dagger.Binds
 import dagger.Module
@@ -51,6 +53,12 @@ interface LMODroidModule {
     @IntoMap
     @StringKey(CaffeineTile.TILE_SPEC)
     fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+
+    /** Inject CellularTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CellularTile.TILE_SPEC)
+    fun bindCellularTile(cellularTile: CellularTile): QSTileImpl<*>
 
     /** Inject HeadsUpTile into tileMap in QSModule */
     @Binds
@@ -81,4 +89,10 @@ interface LMODroidModule {
     @IntoMap
     @StringKey(VPNTetheringTile.TILE_SPEC)
     fun bindVPNTetheringTile(vpnTetheringTile: VPNTetheringTile): QSTileImpl<*>
+
+    /** Inject WifiTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(WifiTile.TILE_SPEC)
+    fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
 }
