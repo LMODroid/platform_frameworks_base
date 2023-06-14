6bfe3bf10c6441937e9fa85793418fa9c6f393b5
Michael Bestas <mkbestas@gmail.com>
2023-06-14 11:53:18 +0300

    fixup! SystemUI: Add powershare QS tile
    Change-Id: I4042a2cb04ee05a41993ed24cd64fd509a04673f
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- packages/SystemUI/src/com/android/systemui/libremobileos/LMODroidModule.kt
+++ packages/SystemUI/src/com/android/systemui/libremobileos/LMODroidModule.kt
@@ -22,6 +22,7 @@ import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.HeadsUpTile
+import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 
@@ -55,6 +56,12 @@ interface LMODroidModule {
     @IntoMap
     @StringKey(HeadsUpTile.TILE_SPEC)
     fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
+    /** Inject PowerShareTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(PowerShareTile.TILE_SPEC)
+    fun bindPowerShareTile(powerShareTile: PowerShareTile): QSTileImpl<*>
 
     /** Inject SyncTile into tileMap in QSModule */
     @Binds
