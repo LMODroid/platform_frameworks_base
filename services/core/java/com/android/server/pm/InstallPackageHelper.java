LMODroid/platform_frameworks_base

commit 37fb7460ea78c792d0502c031996458b56896628

base: Add support for parallel space
This adds initial support for running multiple instances of the same
app at the same time. Basically this is done by making use of multiuser.

Further work is needed to make it perfect.

Change-Id: Ib385753df5778a25facd39166abd8b1e29ed7e91
Signed-off-by: LibXZR <i@xzr.moe>
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

services/core/java/com/android/server/pm/PackageManagerShellCommand.java
@@ -3031,7 +3031,9 @@ class PackageManagerShellCommand extends ShellCommand {
     private static class InstallParams {
         SessionParams sessionParams;
         String installerPackageName;
-        int userId = UserHandle.USER_ALL;
+        // Make sure it won't be installed into parallel users
+        // via `adb install` command by default.
+        int userId = UserHandle.USER_CURRENT;
         long stagedReadyTimeoutMs = DEFAULT_STAGED_READY_TIMEOUT_MS;
     }
 

core/java/android/content/Intent.java
@@ -4127,6 +4127,13 @@ public class Intent implements Parcelable, Cloneable {
             "android.intent.action.PROFILE_INACCESSIBLE";
 
     /**
+     * Broadcast sent to the parallel owner user when parallel space info has been refreshed.
+     * @hide
+     */
+    public static final String ACTION_PARALLEL_SPACE_CHANGED =
+            "android.intent.action.PARALLEL_SPACE_CHANGED";
+
+    /**
      * Broadcast sent to the system user when the 'device locked' state changes for any user.
      * Carries an extra {@link #EXTRA_USER_HANDLE} that specifies the ID of the user for which
      * the device was locked or unlocked.

core/java/android/content/pm/UserInfo.java
@@ -159,6 +159,14 @@ public class UserInfo implements Parcelable {
     public static final int FLAG_EPHEMERAL_ON_CREATE = 0x00002000;
 
     /**
+     * Indicates that this user is a parallel space.
+     * Use the flag space in reverse order to make sure it won't be broke in future updates.
+     *
+     * @hide
+     */
+    public static final int FLAG_PARALLEL = 0x80000000;
+
+    /**
      * @hide
      */
     @IntDef(flag = true, prefix = "FLAG_", value = {
@@ -175,7 +183,8 @@ public class UserInfo implements Parcelable {
             FLAG_FULL,
             FLAG_SYSTEM,
             FLAG_PROFILE,
-            FLAG_EPHEMERAL_ON_CREATE
+            FLAG_EPHEMERAL_ON_CREATE,
+            FLAG_PARALLEL
     })
     @Retention(RetentionPolicy.SOURCE)
     public @interface UserInfoFlag {
@@ -213,6 +222,14 @@ public class UserInfo implements Parcelable {
     @UnsupportedAppUsage
     public int profileGroupId;
     public int restrictedProfileParentId;
+
+    /**
+     * If this user is a parallel space, then we should point to its parent user here.
+     * Some unwanted features are bound to the `profileGroupId` above, so it's better
+     * to save the parent here in a new variable.
+     * @hide
+     */
+    public int parallelParentId;
 
     /**
      * Index for distinguishing different profiles with the same parent and user type for the
@@ -274,6 +291,7 @@ public class UserInfo implements Parcelable {
         this.iconPath = iconPath;
         this.profileGroupId = NO_PROFILE_GROUP_ID;
         this.restrictedProfileParentId = NO_PROFILE_GROUP_ID;
+        this.parallelParentId = UserHandle.USER_NULL;
     }
 
     /**
@@ -343,6 +361,11 @@ public class UserInfo implements Parcelable {
         return UserManager.isUserTypeCloneProfile(userType);
     }
 
+    /** @hide */
+    public boolean isParallel() {
+        return (flags & FLAG_PARALLEL) != 0;
+    }
+
     @UnsupportedAppUsage
     public boolean isEnabled() {
         return (flags & FLAG_DISABLED) != FLAG_DISABLED;
@@ -399,7 +422,7 @@ public class UserInfo implements Parcelable {
             // Don't support switching to pre-created users until they become "real" users.
             return false;
         }
-        return !isProfile();
+        return !isProfile() && !isParallel();
     }
 
     /**
@@ -449,6 +472,7 @@ public class UserInfo implements Parcelable {
         convertedFromPreCreated = orig.convertedFromPreCreated;
         profileGroupId = orig.profileGroupId;
         restrictedProfileParentId = orig.restrictedProfileParentId;
+        parallelParentId = orig.parallelParentId;
         guestToRemove = orig.guestToRemove;
         profileBadge = orig.profileBadge;
     }
@@ -506,6 +530,7 @@ public class UserInfo implements Parcelable {
         dest.writeBoolean(guestToRemove);
         dest.writeInt(restrictedProfileParentId);
         dest.writeInt(profileBadge);
+        dest.writeInt(parallelParentId);
     }
 
     @UnsupportedAppUsage
@@ -535,5 +560,6 @@ public class UserInfo implements Parcelable {
         guestToRemove = source.readBoolean();
         restrictedProfileParentId = source.readInt();
         profileBadge = source.readInt();
+        parallelParentId = source.readInt();
     }
 }

core/java/android/os/UserManager.java
@@ -173,6 +173,18 @@ public class UserManager {
     public static final String USER_TYPE_SYSTEM_HEADLESS = "android.os.usertype.system.HEADLESS";
 
     /**
+     * User type representing a parallel space without the ability of sharing media.
+     * @hide
+     */
+    public static final String USER_TYPE_PARALLEL_DEFAULT = "android.os.usertype.parallel.DEFAULT";
+
+    /**
+     * User type representing a parallel space with the ability of sharing media.
+     * @hide
+     */
+    public static final String USER_TYPE_PARALLEL_SHARE = "android.os.usertype.parallel.SHARE";
+
+    /**
      * Flag passed to {@link #requestQuietModeEnabled} to request disabling quiet mode only if
      * there is no need to confirm the user credentials. If credentials are required to disable
      * quiet mode, {@link #requestQuietModeEnabled} will do nothing and return {@code false}.
@@ -4084,7 +4096,7 @@ public class UserManager {
         int aliveUserCount = 0;
         for (int i = 0; i < totalUserCount; i++) {
             UserInfo user = users.get(i);
-            if (!user.isGuest()) {
+            if (!user.isGuest() && !user.isParallel()) {
                 aliveUserCount++;
             }
         }
@@ -4603,10 +4615,6 @@ public class UserManager {
      * @hide
      */
     public boolean hasBadge(@UserIdInt int userId) {
-        if (!isProfile(userId)) {
-            // Since currently only profiles actually have badges, we can do this optimization.
-            return false;
-        }
         try {
             return mService.hasBadge(userId);
         } catch (RemoteException re) {

core/java/com/android/internal/libremobileos/app/IParallelSpaceManager.aidl
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.libremobileos.app;
+
+import android.content.pm.UserInfo;
+
+/** @hide */
+interface IParallelSpaceManager {
+    int create(String name, boolean shareMedia);
+    int remove(int userId);
+    UserInfo[] getUsers();
+    int duplicatePackage(String packageName, int userId);
+    int removePackage(String packageName, int userId);
+}

core/java/com/android/internal/libremobileos/app/ParallelSpaceManager.java
@@ -1,0 +1,132 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.libremobileos.app;
+
+import android.content.Context;
+import android.content.pm.UserInfo;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.UserHandle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Simple wrapper for ParallelSpaceManagerService.
+ * It requires android.permission.MANAGE_PARALLEL_SPACES.
+ * @hide
+ */
+public final class ParallelSpaceManager {
+
+    public static final String SERVICE_NAME = "parallel";
+    private static final String TAG = "ParallelSpaceManager";
+
+    private static ParallelSpaceManager sParallelSpaceManager;
+    private IParallelSpaceManager mParallelSpaceManager;
+
+    private ParallelSpaceManager() {
+        mParallelSpaceManager = IParallelSpaceManager.Stub.asInterface(
+                ServiceManager.getService(SERVICE_NAME));
+        if (mParallelSpaceManager == null)
+            throw new RuntimeException("Unable to get ParallelSpaceManagerService.");
+    }
+
+    public static ParallelSpaceManager getInstance() {
+        if (sParallelSpaceManager != null)
+            return sParallelSpaceManager;
+        sParallelSpaceManager = new ParallelSpaceManager();
+        return sParallelSpaceManager;
+    }
+
+    private int create(String name, boolean shareMedia) {
+        try {
+            return mParallelSpaceManager.create(name, shareMedia);
+        } catch (RemoteException e) {
+            throw new RuntimeException("Failed when create(): " + e);
+        }
+    }
+
+    /**
+     * Create a parallel space. Returns user id on success, -1 otherwise.
+     */
+    public int create(String name) {
+        return create(name, false);
+    }
+
+    /**
+     * Remove a parallel space. Returns 0 on success.
+     */
+    public int remove(int userId) {
+        try {
+            return mParallelSpaceManager.remove(userId);
+        } catch (RemoteException e) {
+            throw new RuntimeException("Failed when remove(): " + e);
+        }
+    }
+
+    /**
+     * Get a UserInfo list of current existing parallel users.
+     */
+    public List<UserInfo> getParallelUsers() {
+        try {
+            return new ArrayList<>(Arrays.asList(mParallelSpaceManager.getUsers()));
+        } catch (RemoteException e) {
+            throw new RuntimeException("Failed when getUsers(): " + e);
+        }
+    }
+
+    /**
+     * Get a user id list of current existing parallel users.
+     */
+    public List<Integer> getParallelUserIds() {
+        ArrayList<Integer> ret = new ArrayList<>();
+        getParallelUsers().forEach(user -> ret.add(user.id));
+        return ret;
+    }
+
+    /**
+     * Get a UserHandle list of current existing parallel users.
+     */
+    public List<UserHandle> getParallelUserHandles() {
+        ArrayList<UserHandle> ret = new ArrayList<>();
+        getParallelUserIds().forEach(id -> ret.add(UserHandle.of(id)));
+        return ret;
+    }
+
+    /**
+     * Install an existing package to target user. Returns 0 on success.
+     */
+    public int duplicatePackage(String packageName, int userId) {
+        try {
+            return mParallelSpaceManager.duplicatePackage(packageName, userId);
+        } catch (RemoteException e) {
+            throw new RuntimeException("Failed when duplicatePackage(): " + e);
+        }
+    }
+
+    /**
+     * Remove a package from target user. Returns 0 on success.
+     */
+    public int removePackage(String packageName, int userId) {
+        try {
+            return mParallelSpaceManager.removePackage(packageName, userId);
+        } catch (RemoteException e) {
+            throw new RuntimeException("Failed when removePackage(): " + e);
+        }
+    }
+}

core/res/AndroidManifest.xml
@@ -3019,6 +3019,10 @@
     <permission android:name="android.permission.MANAGE_USERS"
         android:protectionLevel="signature|privileged" />
 
+    <!-- @hide Allow managing parallel spaces or receiving related broadcasts. -->
+    <permission android:name="android.permission.MANAGE_PARALLEL_SPACES"
+        android:protectionLevel="signature|privileged" />
+
     <!-- @SystemApi @hide Allows an application to create, remove users and get the list of
          users on the device. Applications holding this permission can create users (including
          normal, restricted, guest, managed, and demo users) and can optionally endow them with the

core/res/res/drawable/ic_parallel_badge.xml
@@ -1,0 +1,31 @@
+<!--
+  Copyright (C) 2022 Project Kaleidoscope
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+
+<vector android:height="24dp" android:viewportHeight="1024"
+    android:viewportWidth="1024" android:width="24dp" xmlns:android="http://schemas.android.com/apk/res/android">
+    <path android:fillColor="#00000000"
+        android:pathData="M186.1,555.7L537.2,212.5"
+        android:strokeColor="#000" android:strokeWidth="100"/>
+    <path android:fillColor="#00000000"
+        android:pathData="M811.8,493.4L460.6,836.6"
+        android:strokeColor="#000" android:strokeWidth="100"/>
+    <path android:fillColor="#00000000"
+        android:pathData="M537.2,212.6c97,-94.8 237,-108.7 312.8,-31.2s58.7,217.3 -38.3,312.1"
+        android:strokeColor="#000" android:strokeWidth="100"/>
+    <path android:fillColor="#FF000000"
+        android:pathData="M323.4,696.1m-196.4,0a196.4,196.4 0,1 1,392.8 0a196.4,196.4 0,1 1,-392.8 0"
+        android:strokeColor="#000" android:strokeWidth="100"/>
+</vector>

commit 6951af55377a3539b4820847b1cf8fa1a7ad950e

pm: Restore the default behavior of `pm install`
Commit 37fb7460 ("Add support for parallel space") changed the default
behavior of `pm install` command to install only to the current user
rather than all users (since that would also include parallel space
user which is not ideal). However, that is incompliant with CTS, which
expects `pm install` to install the apk to the primary and secondary
user when the user id is not specified explicitly.

Just revert back to the original behaviour and filter out parallel
space user ids when installing the package for all users.

Fixes: 37fb7460ea78c792d0502c031996458b56896628 ("base: Add support for parallel space")
Test: CtsContentTestCases:PackageManagerShellCommandMultiUserTest
Change-Id: I432cb66f34cbb7b0beb47601e63e51a99c9050cf

services/core/java/com/android/server/pm/InstallPackageHelper.java
@@ -113,6 +113,7 @@ import android.content.pm.PermissionInfo;
 import android.content.pm.SharedLibraryInfo;
 import android.content.pm.Signature;
 import android.content.pm.SigningDetails;
+import android.content.pm.UserInfo;
 import android.content.pm.VerifierInfo;
 import android.content.pm.dex.DexMetadataHelper;
 import android.content.pm.parsing.result.ParseResult;
@@ -2093,6 +2094,14 @@ final class InstallPackageHelper {
                     // The caller explicitly specified INSTALL_ALL_USERS flag.
                     // Thus, updating the settings to install the app for all users.
                     for (int currentUserId : allUsers) {
+                        UserInfo userInfo =
+                                UserManagerService.getInstance().getUserInfo(currentUserId);
+                        if (userInfo != null && userInfo.isParallel()) {
+                            if (DEBUG_INSTALL) {
+                                Slog.d(TAG, "User " + currentUserId + " is parallel space, skip install");
+                            }
+                            break;
+                        }
                         ps.setInstalled(true, currentUserId);
                         ps.setEnabled(COMPONENT_ENABLED_STATE_DEFAULT, userId,
                                 installerPackageName);

services/core/java/com/android/server/pm/PackageManagerShellCommand.java
@@ -3031,9 +3031,7 @@ class PackageManagerShellCommand extends ShellCommand {
     private static class InstallParams {
         SessionParams sessionParams;
         String installerPackageName;
-        // Make sure it won't be installed into parallel users
-        // via `adb install` command by default.
-        int userId = UserHandle.USER_CURRENT;
+        int userId = UserHandle.USER_ALL;
         long stagedReadyTimeoutMs = DEFAULT_STAGED_READY_TIMEOUT_MS;
     }
 