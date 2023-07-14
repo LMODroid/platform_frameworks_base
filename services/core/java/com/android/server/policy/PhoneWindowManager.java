ff93cac40f7f221bf0f7806953d3792871b4e0e7
someone5678 <nemui3353@gmail.com>
2023-07-14 17:26:19 +0900

    Implement edge long swipe gesture to new back gesture affordance
    Author: Nico <nicorg2529@gmail.com>
    Date:   Sun Mar 8 11:39:47 2020 +0000
    
        Implement edge long swipe gesture [1/3]
    
        The gesture will activate if user executes edge swipe for a long
        horizontal motion. The triggered action is configurable in Button
        Settings.
    
        Reference:
        https://gerrit.dirtyunicorns.com/q/topic:%22back-longswipe-actions%22
    
        Change-Id: Ie1bbe6645b6a00d346af60d6bb5e4d584997d6e9
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu Nov 25 17:39:29 2021 +0100
    
        Use custom flag for edge long swipe gesture
    
        Previously we were using same flags that'd be used for regular back key
        long press thus being unable to determine whether the event was sent
        from EdgeBackGestureHandle or not.
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/4194
        Change-Id: I5c4fd455f581ac5c9c5e3a146095be33e82e8d6e
    
    Author: someone5678 <nemui3353@gmail.com>
    Date:   Fri, 14 Jul 2023 17:26:19 +0900
    
        Adapt and fix from original long swipe gesture impl
    
        Change-Id: I22c6442299df0cf15d046718fa40df6fb6aee07c
        Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    Author: someone5678 <nemui3353@gmail.com>
    Date:   Sun Jul 30 20:55:13 2023 +0900
    
        Flip back gesture canvas if started from left
    
        * Better than constant right arrow direction
    
        Change-Id: I45108f185130d6f778660ba11f3beb758826ba99
        Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    Author: someone5678 <nemui3353@gmail.com>
    Date:   Tue Aug 1 12:25:00 2023 +0900
    
        Always add second arrow in front on long swipe
    
        * After last commit if long swipe gesture started from left, second arrow is added
          in back instead of front.
    
        * This looks unnatural and causes inconsistency with right.
    
        * Hardcode to -1 so that second arrow always added in front of first one.
    
        Change-Id: Ia043a749218ab40c40520736fa54ffaa26919137
        Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    Change-Id: I6a54d781b64aeec278b9f1be4a42bcfaf681b160

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -641,6 +641,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private Action mAssistLongPressAction;
     private Action mAppSwitchPressAction;
     private Action mAppSwitchLongPressAction;
+    private Action mEdgeLongSwipeAction;
 
     // support for activating the lock screen while the screen is on
     private HashSet<Integer> mAllowLockscreenWhenOnDisplays = new HashSet<>();
@@ -775,6 +776,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private PendingIntent mTorchOffPendingIntent;
 
     private LineageHardwareManager mLineageHardware;
+
+    private boolean mLongSwipeDown;
 
     private class PolicyHandler extends Handler {
         @Override
@@ -972,6 +975,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     LMOSettings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    LMOSettings.System.KEY_EDGE_LONG_SWIPE_ACTION), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     LMOSettings.System.HOME_WAKE_SCREEN), false, this,
@@ -2887,6 +2893,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final Resources res = mContext.getResources();
 
         // Initialize all assignments to sane defaults.
+        mEdgeLongSwipeAction = Action.NOTHING;
+
         mMenuPressAction = Action.MENU;
 
         mMenuLongPressAction = Action.fromIntSafe(res.getInteger(
@@ -2955,6 +2963,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mAppSwitchLongPressAction = Action.fromSettings(resolver,
                 LMOSettings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION,
                 mAppSwitchLongPressAction);
+
+        mEdgeLongSwipeAction = Action.fromSettings(resolver,
+                LMOSettings.System.KEY_EDGE_LONG_SWIPE_ACTION,
+                mEdgeLongSwipeAction);
 
         mShortPressOnWindowBehavior = SHORT_PRESS_WINDOW_NOTHING;
         if (mPackageManager.hasSystemFeature(FEATURE_PICTURE_IN_PICTURE)) {
@@ -4836,7 +4848,25 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // Handle special keys.
         switch (keyCode) {
             case KeyEvent.KEYCODE_BACK: {
+                boolean isLongSwipe = (event.getFlags() & KeyEvent.FLAG_LONG_SWIPE) != 0;
                 logKeyboardSystemsEventOnActionUp(event, KeyboardLogEvent.BACK);
+
+                if (mLongSwipeDown && isLongSwipe && !down) {
+                    // Trigger long swipe action
+                    performKeyAction(mEdgeLongSwipeAction, event);
+                    // Reset long swipe state
+                    mLongSwipeDown = false;
+                    // Don't pass back press to app
+                    result &= ~ACTION_PASS_TO_USER;
+                    break;
+                }
+                mLongSwipeDown = isLongSwipe && down;
+                if (mLongSwipeDown) {
+                    // Don't pass back press to app
+                    result &= ~ACTION_PASS_TO_USER;
+                    break;
+                }
+
                 if (down) {
                     mBackKeyHandled = false;
 
