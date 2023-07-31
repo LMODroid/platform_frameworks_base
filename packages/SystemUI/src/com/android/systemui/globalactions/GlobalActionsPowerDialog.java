1d036ccfe97f18089c54ee18dcfa8e135cccb56f
Dhina17 <dhinalogu@gmail.com>
2023-07-31 20:54:24 +0530

    GlobalActionsDialog: Set dim only when blur is enabled
    - Also bring back Scrim Alpha value.
    - Matches with AOSP style when blur is disabled.
    
    Change-Id: Icafaac08b8e14ed5aa8c9b99b4c22be6babbf860

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsPowerDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsPowerDialog.java
@@ -81,7 +81,10 @@ public class GlobalActionsPowerDialog {
                 com.android.systemui.res.R.drawable.global_actions_lite_background,
                 context.getTheme()));
         window.addFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
-        window.setDimAmount(blurUtils.supportsBlursOnWindows() ? 0.54f : 0.88f);
+        // Set dim only when blur is enabled.
+        if (blurUtils.supportsBlursOnWindows()) {
+             window.setDimAmount(0.54f);
+        }
 
         return dialog;
     }
