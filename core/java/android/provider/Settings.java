db8611558087e566883a02fac68b21e9a8e1871f
Dhina17 <dhinalogu@gmail.com>
2023-09-15 14:18:30 +0530

    GameManagerService: Set device_config property on behalf of GameSpace
    GTS don't allow any app to have the android.permission.WRITE_DEVICE_CONFIG permission
    i.e we can't modify device_config property from our game space.
    
    So write the property to our custom Settings.Secure.GAME_OVERLAY.
    Then observe the value changes and set the property to the
    device_config from here.
    
    Since GameManagerService is a part of system_server,
    GTS will be happy.
    
    Change-Id: I0ebbcd6188411a583fa53904e6153482f342a03e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11767,6 +11767,15 @@ public final class Settings {
                 RING_HOME_BUTTON_BEHAVIOR_DO_NOTHING;
 
         /**
+         * Our GameSpace can't write to device_config directly [GTS]
+         * Use this as intermediate to pass device_config property
+         * from our GameSpace to com.android.server.app.GameManagerService
+         * so we can set the device_config property from there.
+         * @hide
+         */
+        public static final String GAME_OVERLAY = "game_overlay";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
