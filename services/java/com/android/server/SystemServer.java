b6dc2e10628fde3a3ba502025f220d115f57a55d
Erfan Abdi <erfangplus@gmail.com>
2023-10-13 17:07:56 +0330

    base: Include lmodroid framework
    Change-Id: Ia364219e450a2ddf05bf71ae8efe76b977a747c5

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -311,6 +311,8 @@ import java.util.TreeSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
+import com.libremobileos.server.LMOSystemServer;
+
 /**
  * Entry point to {@code system_server}.
  */
@@ -2870,6 +2872,10 @@ public final class SystemServer implements Dumpable {
         // Perfetto TracingServiceProxy
         t.traceBegin("startTracingServiceProxy");
         mSystemServiceManager.startService(TracingServiceProxy.class);
+        t.traceEnd();
+
+        t.traceBegin("startLMODroidServices");
+        LMOSystemServer.startServices(context, mSystemServiceManager);
         t.traceEnd();
 
         // It is now time to start up the app processes...
