a436d6c6345b93d243e5f505f7e5e5a65af0a796
Erfan Abdi <erfangplus@gmail.com>
2023-10-13 17:07:56 +0330

    base: Include lmodroid framework
    Change-Id: Ia364219e450a2ddf05bf71ae8efe76b977a747c5

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -321,6 +321,8 @@ import java.util.TreeSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
+import com.libremobileos.server.LMOSystemServer;
+
 /**
  * Entry point to {@code system_server}.
  */
@@ -2938,6 +2940,10 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(DynamicInstrumentationManagerService.class);
             t.traceEnd();
         }
+
+        t.traceBegin("startLMODroidServices");
+        LMOSystemServer.startServices(context, mSystemServiceManager);
+        t.traceEnd();
 
         // It is now time to start up the app processes...
 
