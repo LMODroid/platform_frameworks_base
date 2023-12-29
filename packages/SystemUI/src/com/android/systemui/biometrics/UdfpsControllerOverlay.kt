c9dd45c83e64811dc77c7b53f6f95f6b0b63f620
electimon <electimon@gmail.com>
2023-12-30 03:21:58 +0800

    UdfpsController/Overlay: Delay by frames rather than milliseconds
    Co-authored-by: nift4 <nift4@protonmail.com>
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: Iea6082657d6c99cf1ecf25814759822d3d9de631

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
@@ -119,7 +119,8 @@ constructor(
     private val powerInteractor: PowerInteractor,
     @Application private val scope: CoroutineScope,
 ) {
-    private var frame: View? = null
+    var frame: View? = null
+        private set
     private var isDimmed = false
     private var hideOnUndim = false
     private val currentStateUpdatedToOffAodOrDozing: Flow<Unit> =
