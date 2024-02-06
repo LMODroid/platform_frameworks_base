0199589c285fac446c9b71b8c6f1b9c744868789
electimon <electimon@gmail.com>
2024-02-06 16:43:10 +0000

    SystemUI: Add config for pulsing on pickup
    Change-Id: I37563d5a1da45ce473e8ab8570ff0fd30bdd6338
    Signed-off-by: electimon <electimon@gmail.com>

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -119,6 +119,7 @@ public class DozeTriggers implements DozeMachine.Part {
     private boolean mWantTouchScreenSensors;
     private boolean mWantSensors;
     private boolean mInAod;
+    private boolean mPulseOnPickup;
 
     private final UserTracker.Callback mUserChangedCallback =
             new UserTracker.Callback() {
@@ -231,6 +232,7 @@ public class DozeTriggers implements DozeMachine.Part {
         mKeyguardStateController = keyguardStateController;
         mUserTracker = userTracker;
         mSelectedUserInteractor = selectedUserInteractor;
+        mPulseOnPickup = mContext.getResources().getBoolean(R.bool.doze_pulse_on_pick_up);
     }
 
     @Override
@@ -365,7 +367,12 @@ public class DozeTriggers implements DozeMachine.Part {
                         mDozeLog.traceSensorEventDropped(pulseReason, "keyguard occluded");
                         return;
                     }
-                    gentleWakeUp(pulseReason);
+                    if (mPulseOnPickup) {
+                        requestPulse(pulseReason, sensorPerformedProxCheck /* alreadyPerformedProxCheck */,
+                                null /* onPulseSuppressedListener */);
+                    } else {
+                        gentleWakeUp(pulseReason);
+                    }
                 } else if (isUdfpsLongPress) {
                     if (canPulse(mMachine.getState(), true)) {
                         mDozeLog.d("updfsLongPress - setting aodInterruptRunnable to run when "
