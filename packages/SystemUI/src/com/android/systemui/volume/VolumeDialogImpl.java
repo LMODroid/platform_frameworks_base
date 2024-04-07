61c0115fc6d78bb09c576196ae6e45d63d22323b
danielml <daniel@danielml.dev>
2024-04-07 23:42:41 +0200

    VolumeDialogImpl: Handle cases where media controller is not available
    As of QPR2, we can't crate a media output dialog with empty package
    name unless the use_media_router2_for_info_media_manager flag is
    enabled.
    
    When the flag is enabled, we can always show the dialog by passing
    the actual package name, or null if the media controller is not
    available.
    
    If the flag is disabled, we can only show the dialog if the media
    controller is available.
    
    Change-Id: Ic8d08905bf55be91b547708b316ef7c96c81c492
    Signed-off-by: danielml <daniel@danielml.dev>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -129,6 +129,7 @@ import com.android.internal.graphics.drawable.BackgroundBlurDrawable;
 import com.android.internal.jank.InteractionJankMonitor;
 import com.android.internal.view.RotationPolicy;
 import com.android.settingslib.Utils;
+import com.android.settingslib.media.flags.Flags;
 import com.android.systemui.Dependency;
 import com.android.systemui.Dumpable;
 import com.android.systemui.Prefs;
@@ -1473,8 +1474,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             mRoundedBorderBottom.setVisibility(!showSettings ? VISIBLE : GONE);
         }
         if (mSettingsView != null) {
-            mSettingsView.setVisibility(
-                    showSettings && (isMediaControllerAvailable() || isBluetoothA2dpConnected())
+            mSettingsView.setVisibility(showSettings && (isMediaControllerAvailable() ||
+                    (Flags.useMediaRouter2ForInfoMediaManager() && isBluetoothA2dpConnected()))
                             ? VISIBLE
                             : GONE);
         }
@@ -1483,7 +1484,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                 Events.writeEvent(Events.EVENT_SETTINGS_CLICK);
                 String packageName = isMediaControllerAvailable()
                         ? getActiveLocalMediaController().getPackageName()
-                        : "";
+                        : null;
                 mMediaOutputDialogFactory.create(packageName, true, mDialogView);
                 dismissH(DISMISS_REASON_SETTINGS_CLICKED);
             });
