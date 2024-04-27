689776be1a8dcaacaeb301871c88ac7cf7ef4527
Dhina17 <dhinalogu@gmail.com>
2024-04-27 19:14:22 +0000

    SystemUI: Don't show parallel users on QS user switcher
    Change-Id: I234cb314662572db0c86334c6b9676fcbac50917

--- packages/SystemUI/src/com/android/systemui/user/data/repository/UserRepository.kt
+++ packages/SystemUI/src/com/android/systemui/user/data/repository/UserRepository.kt
@@ -229,7 +229,9 @@ constructor(
 
     override fun refreshUsers() {
         applicationScope.launch {
-            val result = withContext(backgroundDispatcher) { manager.aliveUsers }
+            val result = withContext(backgroundDispatcher) {
+                manager.aliveUsers.filter { !it.isParallel() } // Ignore parallel users
+            }
 
             if (result != null) {
                 _userInfos.value =
