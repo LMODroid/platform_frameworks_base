6da61b1daabf0f5c7dac1e2232940c5a549fe34f
nift4 <nift4@protonmail.com>
2024-09-07 18:09:15 +0200

    Allow forcing all ongoing notifications to be non-dismissible [1/4]
    Change-Id: Ie58b202bc2e335b14f8f8178623050ecbfaf40b4

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -399,6 +399,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
+import com.libremobileos.providers.LMOSettings;
+
 /** {@hide} */
 public class NotificationManagerService extends SystemService {
     public static final String TAG = "NotificationService";
@@ -709,6 +711,7 @@ public class NotificationManagerService extends SystemService {
     private ConditionProviders mConditionProviders;
     private NotificationUsageStats mUsageStats;
     private boolean mLockScreenAllowSecureNotifications = true;
+    private boolean mForceNonDismissible = false;
     final ArrayMap<String, ArrayMap<Integer,
             RemoteCallbackList<ICallNotificationEventCallback>>>
             mCallNotificationEventCallbacks = new ArrayMap<>();
@@ -2115,6 +2118,8 @@ public class NotificationManagerService extends SystemService {
                 = Secure.getUriFor(Secure.NOTIFICATION_BUBBLES);
         private final Uri NOTIFICATION_RATE_LIMIT_URI
                 = Settings.Global.getUriFor(Settings.Global.MAX_NOTIFICATION_ENQUEUE_RATE);
+        private final Uri NOTIFICATION_FORCE_NON_DISMISSIBLE_URI
+                = Settings.System.getUriFor(LMOSettings.System.NOTIFICATION_FORCE_NON_DISMISSIBLE);
         private final Uri NOTIFICATION_HISTORY_ENABLED
                 = Secure.getUriFor(Secure.NOTIFICATION_HISTORY_ENABLED);
         private final Uri NOTIFICATION_SHOW_MEDIA_ON_QUICK_SETTINGS_URI
@@ -2136,6 +2141,8 @@ public class NotificationManagerService extends SystemService {
             resolver.registerContentObserver(NOTIFICATION_BADGING_URI,
                     false, this, USER_ALL);
             resolver.registerContentObserver(NOTIFICATION_RATE_LIMIT_URI,
+                    false, this, USER_ALL);
+            resolver.registerContentObserver(NOTIFICATION_FORCE_NON_DISMISSIBLE_URI,
                     false, this, USER_ALL);
             resolver.registerContentObserver(NOTIFICATION_BUBBLES_URI,
                     false, this, USER_ALL);
@@ -2168,6 +2175,10 @@ public class NotificationManagerService extends SystemService {
             if (uri == null || NOTIFICATION_RATE_LIMIT_URI.equals(uri)) {
                 mMaxPackageEnqueueRate = Settings.Global.getFloat(resolver,
                             Settings.Global.MAX_NOTIFICATION_ENQUEUE_RATE, mMaxPackageEnqueueRate);
+            }
+            if (uri == null || NOTIFICATION_FORCE_NON_DISMISSIBLE_URI.equals(uri)) {
+                mForceNonDismissible = Settings.System.getInt(resolver,
+                        LMOSettings.System.NOTIFICATION_FORCE_NON_DISMISSIBLE, 0) != 0;
             }
             if (uri == null || NOTIFICATION_BADGING_URI.equals(uri)) {
                 mPreferencesHelper.updateBadgingEnabled();
@@ -7825,7 +7836,8 @@ public class NotificationManagerService extends SystemService {
         return notification.isMediaNotification() || isEnterpriseExempted(ai)
                 || notification.isStyle(Notification.CallStyle.class)
                 || isDefaultSearchSelectorPackage(ai.packageName)
-                || isDefaultAdservicesPackage(ai.packageName);
+                || isDefaultAdservicesPackage(ai.packageName)
+                || mForceNonDismissible;
     }
 
     private boolean isDefaultSearchSelectorPackage(String pkg) {
