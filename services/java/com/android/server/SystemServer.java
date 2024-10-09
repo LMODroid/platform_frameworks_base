9c810c309bc36ce6342b52083b7cf3f9ec3289df
Dhina17 <dhinalogu@gmail.com>
2024-10-09 16:56:46 +0530

    SystemServer: Start LMO face unlock service earlier
    We have to start our FaceUnlockService earlier than
    FaceService.
    
    Otherwise our lmo face unlock hal will not
    available at the time of its need in
    com.android.server.biometrics.sensors.face.hidl.HidlToAidlSensorAdapter#getIBiometricsFace()
    
    Change-Id: I8e61b1ab2bfc1f88fb62888860a618e1132c94d3

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2658,6 +2658,10 @@ public final class SystemServer implements Dumpable {
                     = mPackageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT);
 
             if (hasFeatureFace) {
+                t.traceBegin("startLMOFaceUnlockService");
+                LMOSystemServer.startFaceUnlockService(context, mSystemServiceManager);
+                t.traceEnd();
+
                 t.traceBegin("StartFaceSensor");
                 final FaceService faceService =
                         mSystemServiceManager.startService(FaceService.class);
