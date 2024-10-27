93bbf686b0e5b8eddd832e4ac8a04c97e2f43016
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-10-27 08:28:50 +0530

    SystemUI: Use groove for brightness slider
    Inspired by cf95e25 ("Use groove for volume dialog slider")
    
    Change-Id: I3e6273be536e88406b70a9689b61998abb1ea446

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessSliderView.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessSliderView.java
@@ -48,6 +48,8 @@ public class BrightnessSliderView extends LinearLayout {
     private Gefingerpoken mOnInterceptListener;
     @Nullable
     protected Drawable mProgressDrawable;
+    @Nullable
+    private Drawable mProgressBgDrawable;
     protected float mScale = 1f;
     private final Rect mSystemGestureExclusionRect = new Rect();
 
@@ -80,6 +82,7 @@ public class BrightnessSliderView extends LinearLayout {
                     .findDrawableByLayerId(android.R.id.progress);
             LayerDrawable actualProgressSlider = (LayerDrawable) progressSlider.getDrawable();
             mProgressDrawable = actualProgressSlider.findDrawableByLayerId(R.id.slider_foreground);
+            mProgressBgDrawable = progress.findDrawableByLayerId(android.R.id.background);
         } catch (Exception e) {
             // Nothing to do, mProgressDrawable will be null.
         }
@@ -217,10 +220,14 @@ public class BrightnessSliderView extends LinearLayout {
 
     protected void applySliderScale() {
         if (mProgressDrawable != null) {
-            final Rect r = mProgressDrawable.getBounds();
+            Rect r = mProgressDrawable.getBounds();
             int height = (int) (mProgressDrawable.getIntrinsicHeight() * mScale);
             int inset = (mProgressDrawable.getIntrinsicHeight() - height) / 2;
             mProgressDrawable.setBounds(r.left, inset, r.right, inset + height);
+            if (mProgressBgDrawable != null) {
+                r = mProgressBgDrawable.getBounds();
+                mProgressBgDrawable.setBounds(r.left, inset, r.right, inset + height);
+            }
         }
     }
 
