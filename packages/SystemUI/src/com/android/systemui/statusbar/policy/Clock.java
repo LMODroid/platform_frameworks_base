d55d7ab94b9cc1308c7340e1efd0fc6762a02a90
Nick <nift4@protonmail.com>
2024-10-27 08:32:29 +0000

    SystemUI: enable small AM/PM by default
    Change-Id: Id993217f8cd7f26cfcbb326d282ad83e00c8a330
    (cherry picked from commit c5f3e2ca875dbf16ce611209679ce22a522cfa35)

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -112,7 +112,7 @@ public class Clock extends TextView implements
     private static final int AM_PM_STYLE_SMALL   = 1;
     private static final int AM_PM_STYLE_GONE    = 2;
 
-    private int mAmPmStyle = AM_PM_STYLE_GONE;
+    private int mAmPmStyle = AM_PM_STYLE_SMALL;
     private ContentObserver mContentObserver;
     private boolean mShowSeconds;
     private Handler mSecondsHandler;
@@ -152,7 +152,7 @@ public class Clock extends TextView implements
                 0, 0);
         try {
             mAmPmStyle = Settings.System.getInt(mContext.getContentResolver(),
-                    LMOSettings.System.STATUS_BAR_AM_PM, AM_PM_STYLE_GONE);
+                    LMOSettings.System.STATUS_BAR_AM_PM, AM_PM_STYLE_SMALL);
             mContentObserver = new ContentObserver(null) {
                 @Override
                 public void onChange(boolean selfChange, Uri uri) {
@@ -160,7 +160,7 @@ public class Clock extends TextView implements
                             LMOSettings.System.STATUS_BAR_AM_PM).equals(uri)) {
                         mAmPmStyle = Settings.System.getInt(
                                 mContext.getContentResolver(),
-                                LMOSettings.System.STATUS_BAR_AM_PM, AM_PM_STYLE_GONE);
+                                LMOSettings.System.STATUS_BAR_AM_PM, AM_PM_STYLE_SMALL);
                         // Force refresh of dependent variables.
                         mContentDescriptionFormatString = "";
                         mDateTimePatternGenerator = null;
