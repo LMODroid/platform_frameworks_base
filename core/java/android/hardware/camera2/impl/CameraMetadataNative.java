23dd930690c99a6e706f7a6fb3e2a88f8800b83a
Jyoti Bhayana <jbhayana@google.com>
2024-10-28 21:55:35 -0700

    Add multi-client support in camera2
    Add support for multiple clients to access the same camera using
    camera2 api. This is initial set of changes and includes all the new
    API changes as well as implementation of opening the camera in shared
    mode, new open callbacks, newly added characteristics and creating
    shared sesion.
    
    After the merge of these initial changes, it will be followed by
    another set of changes which will include implementation of
    startStreaming and stopStreaming APIs and the capability for multiple
    clients to stream the camera images.
    
    
    Bug:265196098
    API-Coverage-Bug: 377371012
    Test: Tested that a java and native client are able to open the camera
    at the same time and get the required callbacks and they are able to get
    the shared session configuration using newly added characteristics.
    Tested the clientaccessprioritieschanged callback occur appropriately
    when new client connects/disconnects.
    
    Change-Id: Iee01b79a3a4188fa32ce361f829ac8ea62111076

--- core/java/android/hardware/camera2/impl/CameraMetadataNative.java
+++ core/java/android/hardware/camera2/impl/CameraMetadataNative.java
@@ -63,6 +63,7 @@ import android.hardware.camera2.params.OisSample;
 import android.hardware.camera2.params.RecommendedStreamConfiguration;
 import android.hardware.camera2.params.RecommendedStreamConfigurationMap;
 import android.hardware.camera2.params.ReprocessFormatsMap;
+import android.hardware.camera2.params.SharedSessionConfiguration;
 import android.hardware.camera2.params.StreamConfiguration;
 import android.hardware.camera2.params.StreamConfigurationDuration;
 import android.hardware.camera2.params.StreamConfigurationMap;
@@ -861,6 +862,15 @@ public class CameraMetadataNative implements Parcelable {
                         return (T) metadata.getLensIntrinsicSamples();
                     }
                 });
+        sGetCommandMap.put(
+                CameraCharacteristics.SHARED_SESSION_CONFIGURATION.getNativeKey(),
+                        new GetCommand() {
+                    @Override
+                    @SuppressWarnings("unchecked")
+                    public <T> T getValue(CameraMetadataNative metadata, Key<T> key) {
+                        return (T) metadata.getSharedSessionConfiguration();
+                    }
+                });
     }
 
     private int[] getAvailableFormats() {
@@ -1627,6 +1637,22 @@ public class CameraMetadataNative implements Parcelable {
                 jpegRConfigurations, jpegRMinFrameDurations, jpegRStallDurations,
                 highSpeedVideoConfigurations, inputOutputFormatsMap,
                 listHighResolution);
+    }
+
+    private SharedSessionConfiguration getSharedSessionConfiguration() {
+        if (!Flags.cameraMultiClient()) {
+            return null;
+        }
+        Integer sharedSessionColorSpace = getBase(
+                CameraCharacteristics.SHARED_SESSION_COLOR_SPACE);
+        long[] sharedOutputConfigurations = getBase(
+                CameraCharacteristics.SHARED_SESSION_OUTPUT_CONFIGURATIONS);
+
+        if ((sharedSessionColorSpace == null) || (sharedOutputConfigurations == null)) {
+            return null;
+        }
+
+        return new SharedSessionConfiguration(sharedSessionColorSpace, sharedOutputConfigurations);
     }
 
     private StreamConfigurationMap getStreamConfigurationMapMaximumResolution() {
