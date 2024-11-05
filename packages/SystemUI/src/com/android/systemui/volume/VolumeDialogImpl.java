373be3f4794cafddda5c5ce3005c7764f1fae19a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-11-05 12:43:44 +0530

    VolumeDialog: Bail out if app volume view is null
    In TV layout, for example, we don't have app volume in the volume
    dialog layout. Avoid NPE in that case.
    
    Change-Id: I92ba5847f4cb5ad6623da04d228e16480917e93c

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1557,6 +1557,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     }
 
     private void clearAppVolumes() {
+        if (mAppVolumeView == null) {
+            return;
+        }
         mActiveAppRowPackage = null;
         mAppVolumeView.setVisibility(GONE);
         mRows.removeAll(mAppRows);
@@ -1565,11 +1568,14 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     }
 
     private void initAppVolumes() {
+        if (mAppVolumeView == null) {
+            return;
+        }
         clearAppVolumes();
         boolean showAppVolume = Settings.System.getIntForUser(mContext.getContentResolver(),
                 LMOSettings.System.SHOW_APP_VOLUME,
                 0, UserHandle.USER_CURRENT) == 1;
-        if (!showAppVolume || mAppVolumeView == null) {
+        if (!showAppVolume) {
             return;
         }
         boolean appActive = false;
