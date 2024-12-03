ca9fa39cc30efdd01375dd1012f3c80f4e653d50
mayankkk <mayankkk@google.com>
2024-12-03 11:33:45 -0800

    Don't allow non-system uids to use "android" as calling package.
    Bug: 310632322
    Test: atest ./BroadcastsSecurityTest/src/android/app/cts/broadcasts/BroadcastRegistrationTest.java
    Flag: EXEMPT bugfix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:f9619f7ab4496bf98b42f00c622c94e60c898cd1)
    Merged-In: I87517e9b116e5c6ca4ba05729139455c09a31d53
    Change-Id: I87517e9b116e5c6ca4ba05729139455c09a31d53

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -13886,8 +13886,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                 return null;
             }
             if (callerApp.info.uid != SYSTEM_UID
-                    && !callerApp.getPkgList().containsKey(callerPackage)
-                    && !"android".equals(callerPackage)) {
+                    && !callerApp.getPkgList().containsKey(callerPackage)) {
                 throw new SecurityException("Given caller package " + callerPackage
                         + " is not running in process " + callerApp);
             }
