c3749c6507f1ab5ef4af481b5a97842fe8de7bc7
Dhina17 <dhinalogu@gmail.com>
2024-12-04 17:22:28 +0530

    SystemUI: Update UdfpsTouchOverlay sensorRect on updating overlay params
    Fixes an issue that illumination dot showing in wrong
    place when device goes to sleep from landscape orientation.
    
    Test:
    1) Setup screen lock and enroll fingerprint (udfps)
    2) Set screen timeout to 15 seconds (optional for quick testing)
    3) Rotate the device content to landscape
    4) Let it sleep by timeout
    5) Turn on screen and tap the udfps icon
    6) Confirm that it shows the illumination dot in right position
    
    Related to this commit I4a4e85a7437a9a444a4f952fd62e4fe12f56ce5a
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8027
    Change-Id: I8a07eb106283f6666f8a8e2ec16ca39e44a9aa1b

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
@@ -362,6 +362,7 @@ class UdfpsControllerOverlay @JvmOverloads constructor(
         DeviceEntryUdfpsRefactor.isUnexpectedlyInLegacyMode()
         overlayParams = updatedOverlayParams
         sensorBounds = updatedOverlayParams.sensorBounds
+        overlayTouchView?.sensorRect = updatedOverlayParams.sensorBounds
         getTouchOverlay()?.let {
             if (addViewRunnable == null) {
                 // Only updateViewLayout if there's no pending view to add to WM.
