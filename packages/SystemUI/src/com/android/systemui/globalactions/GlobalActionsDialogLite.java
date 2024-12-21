10c4fed5543fe19aaad0d55eaff0ef9f21a5d690
Dhina17 <dhinalogu@gmail.com>
2024-12-21 18:20:02 +0530

    fixup! Power menu styles: Initial checkin for 15 [1/3]
    Fixes no background scrim in global actions when blur
    is disabled.
    
    Change-Id: Icdf786b661fe23773af1ad33ed8a902c68ee67a2

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -2871,7 +2871,6 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             // Inflate the decor view, so the attributes below are not overwritten by the theme.
             window.getDecorView();
             window.setLayout(MATCH_PARENT, MATCH_PARENT);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
             window.addFlags(
                     WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
                             | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
