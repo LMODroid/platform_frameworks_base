ee3ec26c381867ffdab712de1eb21d14bab3a642
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-02-09 10:52:16 +0530

    SystemUI: Do not animate scrim when waking to pulse
    Otherwise we have a white-to-black background animation in tap to pulse
    in light theme.
    
    Change-Id: I8aeed78c42320bad418fe64992072cac230f1184

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
@@ -241,6 +241,9 @@ public enum ScrimState {
             mBlankScreen = mDisplayRequiresBlanking;
             mAnimationDuration = mWakeLockScreenSensorActive
                     ? ScrimController.ANIMATION_DURATION_LONG : ScrimController.ANIMATION_DURATION;
+            if (previousState == OFF || previousState == AOD) {
+                mAnimateChange = false;
+            }
         }
         @Override
         public float getMaxLightRevealScrimAlpha() {
