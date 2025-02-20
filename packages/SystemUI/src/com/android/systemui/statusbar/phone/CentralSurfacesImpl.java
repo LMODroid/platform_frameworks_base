NurKeinKernel/frameworks_base

SystemUI: Set default value of qs transparency as overlay [1/2]
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
Change-Id: I55980bce6387e89df3611f11140ac886b3c0d495
Signed-off-by: Saalim Quadri <danascape@gmail.com>
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -915,7 +915,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                     }
                 } else if (uri.equals(qsTransparency)) {
                     int newValue = Settings.System.getIntForUser(mContext.getContentResolver(),
-                            DerpFestSettings.System.QS_TRANSPARENCY, 100,
+                            DerpFestSettings.System.QS_TRANSPARENCY,
+                            mContext.getResources().getInteger(com.android.internal.R.integer.config_qs_transparency),
                             UserHandle.USER_CURRENT);
                     mContext.getMainExecutor().execute(() -> {
                         mScrimController.setCustomScrimAlpha(newValue);

LMODroid/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -925,7 +925,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                     }
                 } else if (uri.equals(qsTransparency)) {
                     int newValue = Settings.System.getIntForUser(mContext.getContentResolver(),
-                            LMOSettings.System.QS_TRANSPARENCY, 100,
+                            LMOSettings.System.QS_TRANSPARENCY,
+                            mContext.getResources().getInteger(com.android.internal.R.integer.config_qs_transparency),
                             UserHandle.USER_CURRENT);
                     mContext.getMainExecutor().execute(() -> {
                         mScrimController.setCustomScrimAlpha(newValue);