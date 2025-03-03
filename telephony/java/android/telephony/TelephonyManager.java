5fb0d5cdcb12f2104fe68de2ed87ccec84334dbf
Tomasz Wasilczyk <twasilczyk@google.com>
2025-03-03 12:15:33 -0800

    TelephonyManager: use isDeviceVoiceCapable
    Bug: 398045526
    Bug: 390681978
    Bug: 393424911
    Test: CtsTelephonyTestCases:android.telephony.cts.TelephonyManagerTest#testGetEmergencyAssistancePackageName
    Test: CtsTelephonyTestCases
    Flag: EXEMPT bugfix
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:a9ec44ed51a99021974c0748a6727b3b2033ac84
    Merged-In: I3d0d352c4e85b6958b10edb29df4e8a5917507e8
    Change-Id: I3d0d352c4e85b6958b10edb29df4e8a5917507e8

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -681,7 +681,7 @@ public class TelephonyManager {
             case UNKNOWN:
                 modemCount = 1;
                 // check for voice and data support, 0 if not supported
-                if (!isVoiceCapable() && !isSmsCapable() && !isDataCapable()) {
+                if (!isDeviceVoiceCapable() && !isSmsCapable() && !isDataCapable()) {
                     modemCount = 0;
                 }
                 break;
@@ -2729,7 +2729,7 @@ public class TelephonyManager {
      */
     @RequiresFeature(PackageManager.FEATURE_TELEPHONY)
     public int getPhoneType() {
-        if (!isVoiceCapable() && !isDataCapable()) {
+        if (!isDeviceVoiceCapable() && !isDataCapable()) {
             return PHONE_TYPE_NONE;
         }
         return getCurrentPhoneType();
