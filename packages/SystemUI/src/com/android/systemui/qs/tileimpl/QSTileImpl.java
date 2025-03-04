bc8b31a0e42f8aeec1cd3ec543a77f98b06f7e88
Dhina17 <dhinalogu@gmail.com>
2025-03-05 00:14:55 +0530

    fixup! Add setting to allow/disallow interacting with quick settings on secure lockscreen (1/2)
    No point to block QS tiles in keyguard that's not secure i.e Swipe
    Also no way to disable/enable this setting in swipe lockscreen.
    
    Change-Id: I62032dec5b96f1d61d78bd9660e80b28d0a54915

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -328,7 +328,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         mMetricsLogger.write(populate(new LogMaker(category).setType(TYPE_ACTION)
                 .addTaggedData(FIELD_STATUS_BAR_STATE, mStatusBarStateController.getState())));
         mUiEventLogger.logWithInstanceId(event, 0, getMetricsSpec(), getInstanceId());
-        if (!keyguardManager.isKeyguardLocked() ||
+        if (!keyguardManager.isDeviceLocked() ||
                 Settings.Secure.getInt(mContext.getContentResolver(),
                 LMOSettings.Secure.QS_TILES_TOGGLEABLE_ON_LOCK_SCREEN, 1) == 1) {
             sendMessageRunnable.run();
