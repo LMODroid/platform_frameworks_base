a1de6a2f8ab4c2fb549f1d1ce1f302a8298d7fe9
Olivier St-Onge <ostonge@google.com>
2025-03-28 13:37:45 -0400

    Update the edit mode header text
    Test: manually
    Flag: com.android.systemui.qs_ui_refactor_compose_fragment
    Fixes: 389643796
    Change-Id: I6b91a8380eaafa1d4def6f25f68049dfce3960fe
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/EditTile.kt
+++ packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/EditTile.kt
@@ -438,7 +438,7 @@ private fun CurrentTilesGridHeader(
                 }
                 EditModeHeaderState.Idle -> {
                     EditGridCenteredText(
-                        text = stringResource(id = R.string.drag_to_rearrange_tiles)
+                        text = stringResource(id = R.string.select_to_rearrange_tiles)
                     )
                 }
             }
