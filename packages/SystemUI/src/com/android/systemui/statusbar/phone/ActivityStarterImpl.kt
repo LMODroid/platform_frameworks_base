35a7370742cc288fc24239bf9bdb32c182e42664
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-04-15 19:34:36 +0530

    fixup! SystemUI: Add a delay to postQSRunnableDismissingKeyguard
    We don't need the delay when we're already unlocked.
    
    Change-Id: I3445ff9e883f597255b7b299fb4aaa5ac98475aa

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ActivityStarterImpl.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ActivityStarterImpl.kt
@@ -25,6 +25,7 @@ import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.plugins.ActivityStarter
 import com.android.systemui.plugins.ActivityStarter.OnDismissAction
 import com.android.systemui.statusbar.SysuiStatusBarStateController
+import com.android.systemui.statusbar.policy.KeyguardStateController
 import com.android.systemui.util.concurrency.DelayableExecutor
 import dagger.Lazy
 import javax.inject.Inject
@@ -35,6 +36,7 @@ class ActivityStarterImpl
 @Inject
 constructor(
     private val statusBarStateController: SysuiStatusBarStateController,
+    private val keyguardStateController: KeyguardStateController,
     @Main private val mainExecutor: DelayableExecutor,
     legacyActivityStarter: Lazy<LegacyActivityStarterInternalImpl>
 ) : ActivityStarter {
@@ -367,10 +369,11 @@ constructor(
     }
 
     override fun postQSRunnableDismissingKeyguard(runnable: Runnable?) {
+        val delay = if (keyguardStateController.isShowing()) 500 else 0
         postOnUiThread {
             statusBarStateController.setLeaveOpenOnKeyguardHide(true)
             activityStarterInternal.executeRunnableDismissingKeyguard(
-                runnable = { runnable?.let { postOnUiThread(delay = 500, runnable = it) } },
+                runnable = { runnable?.let { postOnUiThread(delay = delay, runnable = it) } },
             )
         }
     }
