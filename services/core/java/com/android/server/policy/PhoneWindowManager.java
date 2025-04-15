4a754f91923842c41dae1258cda9df9dadc55d01
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-04-15 20:03:19 +0530

    base: Show power menu only on dismissing keyguard
    Similar to secure QS tiles.
    
    Change-Id: Ida3339b240e985e8353ea6e9e3f72f9f8b74ccf6

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2041,7 +2041,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mEndCallKeyHandled = true;
             performHapticFeedback(HapticFeedbackConstants.LONG_PRESS,
                     "End Call - Long Press - Show Global Actions");
-            showGlobalActionsInternal();
+            showGlobalActions();
         }
     };
 
@@ -2052,6 +2052,25 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     @Override
     public void showGlobalActions() {
+        if (isKeyguardShowing() && isKeyguardSecure(mCurrentUserId)) {
+            mKeyguardDelegate.dismiss(new IKeyguardDismissCallback.Stub() {
+                @Override
+                public void onDismissSucceeded() {
+                    dispatchShowGlobalActions();
+                }
+
+                @Override
+                public void onDismissError() { }
+
+                @Override
+                public void onDismissCancelled() { }
+            }, /*message=*/ null);
+        } else {
+            dispatchShowGlobalActions();
+        }
+    }
+
+    private void dispatchShowGlobalActions() {
         mHandler.removeMessages(MSG_DISPATCH_SHOW_GLOBAL_ACTIONS);
         mHandler.sendEmptyMessage(MSG_DISPATCH_SHOW_GLOBAL_ACTIONS);
     }
@@ -2704,7 +2723,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mContext.registerReceiver(new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                showGlobalActionsInternal();
+                showGlobalActions();
             }
         }, filter);
 
