e4ef599b03f1498d4bb0b4a8189bc958185803d5
Riddle Hsu <riddlehsu@google.com>
2025-04-28 13:15:08 +0800

    Remove enabled flag use_visible_requested_for_process_tracker
    Bug: 414269923
    Flag: EXEMPT flag removal
    Test: BackgroundActivityLaunchTest
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:01358ba370c7fe020dfbe4328ae65ac3997de6f5
    (cherry picked from commit ce951994091185f2fb6570c8a1e906e1268007a0)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:84ec2555d2f7d7d8dd343d61efb1b54a277e6d34
    Merged-In: I61169d7adafbb8c43a9a1e97fe963cfe50bb90ad
    Change-Id: I61169d7adafbb8c43a9a1e97fe963cfe50bb90ad

--- services/core/java/com/android/server/wm/WindowProcessController.java
+++ services/core/java/com/android/server/wm/WindowProcessController.java
@@ -319,11 +319,6 @@ public class WindowProcessController extends ConfigurationContainer<Configuratio
     private static final int ACTIVITY_STATE_FLAG_HAS_ACTIVITY_IN_VISIBLE_TASK = 1 << 22;
     private static final int ACTIVITY_STATE_FLAG_MASK_MIN_TASK_LAYER = 0x0000ffff;
 
-    private static final int ACTIVITY_STATE_VISIBLE =
-            com.android.window.flags.Flags.useVisibleRequestedForProcessTracker()
-                    ? ACTIVITY_STATE_FLAG_IS_VISIBLE
-                    : ACTIVITY_STATE_FLAG_IS_VISIBLE | ACTIVITY_STATE_FLAG_IS_WINDOW_VISIBLE;
-
     /**
      * The state for oom-adjustment calculation. The higher 16 bits are the activity states, and the
      * lower 16 bits are the task layer rank (see {@link Task#mLayerRank}). This field is written by
@@ -1271,7 +1266,7 @@ public class WindowProcessController extends ConfigurationContainer<Configuratio
         int minTaskLayer = Integer.MAX_VALUE;
         int stateFlags = 0;
         final boolean wasResumed = hasResumedActivity();
-        final boolean wasAnyVisible = (mActivityStateFlags & ACTIVITY_STATE_VISIBLE) != 0;
+        final boolean wasAnyVisible = (mActivityStateFlags & ACTIVITY_STATE_FLAG_IS_VISIBLE) != 0;
         for (int i = mActivities.size() - 1; i >= 0; i--) {
             final ActivityRecord r = mActivities.get(i);
             if (r.isVisible()) {
@@ -1301,8 +1296,7 @@ public class WindowProcessController extends ConfigurationContainer<Configuratio
                     bestInvisibleState = PAUSING;
                     // Treat PAUSING as visible in case the next activity in the same process has
                     // not yet been set as visible-requested.
-                    if (com.android.window.flags.Flags.useVisibleRequestedForProcessTracker()
-                            && r.isVisible()) {
+                    if (r.isVisible()) {
                         stateFlags |= ACTIVITY_STATE_FLAG_IS_VISIBLE;
                     }
                 } else if (state == PAUSED) {
@@ -1340,7 +1334,7 @@ public class WindowProcessController extends ConfigurationContainer<Configuratio
         }
         mActivityStateFlags = stateFlags;
 
-        final boolean anyVisible = (stateFlags & ACTIVITY_STATE_VISIBLE) != 0;
+        final boolean anyVisible = (stateFlags & ACTIVITY_STATE_FLAG_IS_VISIBLE) != 0;
         if (!wasAnyVisible && anyVisible) {
             mAtm.mVisibleActivityProcessTracker.onAnyActivityVisible(this);
             mAtm.mWindowManager.onProcessActivityVisibilityChanged(mUid, true /*visible*/);
