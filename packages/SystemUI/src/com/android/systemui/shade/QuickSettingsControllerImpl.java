0ffa93bd0a3acbfd984925b2f04d083374103e30
Fabián Kozynski <kozynski@google.com>
2025-05-19 16:22:51 -0400

    Early expand media
    This makes media expand early when going from QQS to QS. Same as the expanded that we send to QSFragmentCompose.
    
    Test: abtd run of systemui-media-1-jank-suite
    Fixes: 418068863
    Flag: com.android.systemui.qs_compose_fragment_early_expansion
    
    Change-Id: I2a403486b571b47d52f78ac397a6aa7e82c83049
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
@@ -1692,9 +1692,7 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
             mInitialHeightOnTouch = mExpansionHeight;
             mInitialTouchY = event.getY();
             mInitialTouchX = event.getX();
-            if (qsComposeFragmentEarlyExpansion() && mQs != null) {
-                mQs.setExpanded(true);
-            }
+            maybeSetEarlyExpansion();
         }
         if (!isFullyCollapsed && !isShadeOrQsHeightAnimationRunning) {
             handleDown(event);
@@ -1713,9 +1711,7 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
         }
         if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
             mConflictingExpansionGesture = false;
-            if (qsComposeFragmentEarlyExpansion()) {
-                updateQsState();
-            }
+            maybeResetEarlyExpansion();
         }
         if (action == MotionEvent.ACTION_DOWN && isFullyCollapsed && isExpansionEnabled()) {
             mTwoFingerExpandPossible = true;
@@ -1784,9 +1780,7 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
                 mInitialHeightOnTouch = mExpansionHeight;
                 initVelocityTracker();
                 trackMovement(event);
-                if (qsComposeFragmentEarlyExpansion() && mQs != null) {
-                    mQs.setExpanded(true);
-                }
+                maybeSetEarlyExpansion();
                 break;
 
             case MotionEvent.ACTION_POINTER_UP:
@@ -1824,9 +1818,7 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
                     flingQsWithCurrentVelocity(y,
                             event.getActionMasked() == MotionEvent.ACTION_CANCEL);
                 } else {
-                    if (qsComposeFragmentEarlyExpansion()) {
-                        updateQsState();
-                    }
+                    maybeResetEarlyExpansion();
                     traceQsJank(false,
                             event.getActionMasked() == MotionEvent.ACTION_CANCEL);
                 }
@@ -1912,9 +1904,7 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
                     mInitialTouchY = y;
                     mInitialTouchX = x;
                     mNotificationStackScrollLayoutController.cancelLongPress();
-                    if (qsComposeFragmentEarlyExpansion() && mQs != null) {
-                        mQs.setExpanded(true);
-                    }
+                    maybeSetEarlyExpansion();
                     return true;
                 } else {
                     mShadeLog.logQsTrackingNotStarted(mInitialTouchY, y, h, touchSlop,
@@ -2093,6 +2083,20 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
         float displayDensity = mPanelViewControllerLazy.get().getDisplayDensity();
         mLockscreenGestureLogger.write(gesture,
                 (int) ((y - getInitialTouchY()) / displayDensity), (int) (vel / displayDensity));
+    }
+
+    private void maybeSetEarlyExpansion() {
+        if (QSComposeFragment.isEnabled() && qsComposeFragmentEarlyExpansion() && mQs != null) {
+            mQs.setExpanded(true);
+            mMediaHierarchyManager.setQsExpanded(true);
+        }
+    }
+
+    private void maybeResetEarlyExpansion() {
+        if (QSComposeFragment.isEnabled() && qsComposeFragmentEarlyExpansion()) {
+            updateQsState();
+            mMediaHierarchyManager.setQsExpanded(getExpanded());
+        }
     }
 
     @NeverCompile
