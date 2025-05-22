1bf323d0e17f2c2858b6350f2915517498968a50
burakov <burakov@google.com>
2025-05-22 12:35:27 +0000

    [Dual Shade] Fix KeyguardSecurityContainerControllerTest for DualShade.
    This ensures these unit tests do not fail when Dual Shade is the default
    shade experience in scene container.
    
    Bug: 376411622
    Test: Updated unit tests.
    Flag: com.android.systemui.scene_container
    Change-Id: I32dc5698ad2bdc2d4a53b0d01d1212fc3202500f
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/multivalentTests/src/com/android/keyguard/KeyguardSecurityContainerControllerTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/keyguard/KeyguardSecurityContainerControllerTest.kt
@@ -71,6 +71,7 @@ import com.android.systemui.scene.shared.model.FakeSceneDataSource
 import com.android.systemui.scene.shared.model.Overlays
 import com.android.systemui.scene.shared.model.Scenes
 import com.android.systemui.scene.shared.model.fakeSceneDataSource
+import com.android.systemui.shade.domain.interactor.enableSingleShade
 import com.android.systemui.statusbar.policy.ConfigurationController
 import com.android.systemui.statusbar.policy.DevicePostureController
 import com.android.systemui.statusbar.policy.DeviceProvisionedController
@@ -91,6 +92,7 @@ import com.android.systemui.util.time.FakeSystemClock
 import com.android.systemui.window.domain.interactor.windowRootViewBlurInteractor
 import com.google.common.truth.Truth
 import junit.framework.Assert
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runCurrent
@@ -113,6 +115,7 @@ import org.mockito.Mockito.spy
 import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @SmallTest
 @RunWith(AndroidJUnit4::class)
 @RunWithLooper
@@ -832,6 +835,8 @@ class KeyguardSecurityContainerControllerTest : SysuiTestCase() {
     @EnableSceneContainer
     fun dismissesKeyguard_whenSceneChangesToGone() =
         kosmos.testScope.runTest {
+            kosmos.enableSingleShade()
+            runCurrent()
             // Collect sceneInteractor.currentOverlays so that show/hideOverlay receive updated
             // overlay state during validation
             val currentOverlays by collectLastValue(sceneInteractor.currentOverlays)
