c392392c1153f9d22988e84b2bc4fc091062a3a4
Chandru S <chandruis@google.com>
2025-06-03 11:47:37 -0700

    Set early wakeup signal when shade is expanded through a11y GLOBAL_ACTION_QUICK_SETTINGS
    Most tests use this method to open QS shade instead of swiping down on the screen leading to performance regressions during the test run because the early wakeup signal is not early enough.
    
    Bug: 392209635
    Bug: 408379996
    Bug: 415563164
    Bug: 416225534
    Flag: com.android.systemui.bouncer_ui_revamp
    Test: similar change for notification shade was verified using an ab run.
    Change-Id: I2a7716346a9f593c57734ad76484f5a914ff8842
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1398,6 +1398,11 @@ public final class NotificationPanelViewController implements
             mLockscreenShadeTransitionController.goToLockedShade(
                     /* expandedView= */null, /* needsQSAnimation= */true);
         } else if (isFullyCollapsed()) {
+            if (!SceneContainerFlag.isEnabled() && Flags.bouncerUiRevamp()) {
+                // Mark the bit that indicates that shade is going to expand when shade is opened
+                // through AccessibilityService.GLOBAL_ACTION_QUICK_SETTINGS
+                mWindowRootViewBlurInteractor.setTrackingShadeMotion(true);
+            }
             expand(true /* animate */);
         } else {
             mQsController.traceQsJank(true /* startTracing */, false /* wasCancelled */);
