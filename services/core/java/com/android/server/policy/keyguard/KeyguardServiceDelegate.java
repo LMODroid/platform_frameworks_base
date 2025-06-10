ec66ec8717fa8c38d5f4711136800e010528a2e1
Matt Pietal <mpietal@google.com>
2025-06-10 10:43:15 -0700

    Update user switching processes
    Add a new callback for UserController. If the user is secure, initiate
    a call to SystemUI to lockNow(), and wait for the callback. If the
    callback does not arrive, crash the system after some time.
    
    During user switching, ensure that systemui locks the device immediately
    if necessary and cancels any prior attempt to dismiss.
    
    Also, ensure that any switch is user that occurs after WM has been
    notified that SystemUI is going away is stopped.
    
    Bug: 322157041
    Test: atest KeyguardViewMediatorTest
    Flag: EXEMPT bugfix
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:026ee0c15ca9eff8b5971c70f6801fc3c9418888
    Merged-In: I32699e32fe1dbe94af2abe7e7ade5363a2b6cb55
    Change-Id: I32699e32fe1dbe94af2abe7e7ade5363a2b6cb55

--- services/core/java/com/android/server/policy/keyguard/KeyguardServiceDelegate.java
+++ services/core/java/com/android/server/policy/keyguard/KeyguardServiceDelegate.java
@@ -92,6 +92,8 @@ public class KeyguardServiceDelegate {
         public boolean bootCompleted;
         public int screenState;
         public int interactiveState;
+        boolean doKeyguardTimeoutRequested;
+        Bundle doKeyguardTimeoutRequestedOptions;
 
         private void reset() {
             // Assume keyguard is showing and secure until we know for sure. This is here in
@@ -223,6 +225,12 @@ public class KeyguardServiceDelegate {
             }
             if (mKeyguardState.dreaming) {
                 mKeyguardService.onDreamingStarted();
+            }
+            if (mKeyguardState.doKeyguardTimeoutRequested) {
+                mKeyguardService.doKeyguardTimeout(
+                        mKeyguardState.doKeyguardTimeoutRequestedOptions);
+                mKeyguardState.doKeyguardTimeoutRequested = false;
+                mKeyguardState.doKeyguardTimeoutRequestedOptions = null;
             }
         }
 
@@ -407,6 +415,11 @@ public class KeyguardServiceDelegate {
     public void doKeyguardTimeout(Bundle options) {
         if (mKeyguardService != null) {
             mKeyguardService.doKeyguardTimeout(options);
+        } else {
+            mKeyguardState.doKeyguardTimeoutRequested = true;
+            if (options != null) {
+                mKeyguardState.doKeyguardTimeoutRequestedOptions = options;
+            }
         }
     }
 
