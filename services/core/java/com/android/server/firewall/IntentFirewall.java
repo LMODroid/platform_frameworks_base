1b55192a5e3e64dfe5da6659034fe859fb9aa1f5
Nan Wu <wnan@google.com>
2025-06-27 16:58:21 +0000

    Add ExtraKeyValueFilter to Intent Firewall
    Allows filter intent's extra bundles
    
    Bug: 428733109
    Flag: android.security.enable_intent_firewall_extra_key_value_filter
    Test: ExtraKeyValueFilterTest
    Change-Id: Id6e4b759c81c095c25e6298cc37786000f323b92
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- services/core/java/com/android/server/firewall/IntentFirewall.java
+++ services/core/java/com/android/server/firewall/IntentFirewall.java
@@ -16,6 +16,8 @@
 
 package com.android.server.firewall;
 
+import static android.security.Flags.enableIntentFirewallExtraKeyValueFilter;
+
 import android.annotation.NonNull;
 import android.app.AppGlobals;
 import android.content.ComponentName;
@@ -103,34 +105,67 @@ public class IntentFirewall {
     private List<Rule> mPackageResolver = new ArrayList<>();
 
     static {
-        FilterFactory[] factories = new FilterFactory[] {
-                AndFilter.FACTORY,
-                OrFilter.FACTORY,
-                NotFilter.FACTORY,
+        FilterFactory[] factories;
+        if (enableIntentFirewallExtraKeyValueFilter()) {
+            factories = new FilterFactory[]{
+                    AndFilter.FACTORY,
+                    OrFilter.FACTORY,
+                    NotFilter.FACTORY,
 
-                StringFilter.ACTION,
-                StringFilter.COMPONENT,
-                StringFilter.COMPONENT_NAME,
-                StringFilter.COMPONENT_PACKAGE,
-                StringFilter.DATA,
-                StringFilter.HOST,
-                StringFilter.MIME_TYPE,
-                StringFilter.SCHEME,
-                StringFilter.PATH,
-                StringFilter.SSP,
+                    StringFilter.ACTION,
+                    StringFilter.COMPONENT,
+                    StringFilter.COMPONENT_NAME,
+                    StringFilter.COMPONENT_PACKAGE,
+                    StringFilter.DATA,
+                    StringFilter.HOST,
+                    StringFilter.MIME_TYPE,
+                    StringFilter.SCHEME,
+                    StringFilter.PATH,
+                    StringFilter.SSP,
 
-                CategoryFilter.FACTORY,
-                SenderFilter.FACTORY,
-                SenderPackageFilter.FACTORY,
-                SenderPermissionFilter.FACTORY,
-                TargetFilter.FACTORY,
-                TargetPackageFilter.FACTORY,
-                TargetPermissionFilter.FACTORY,
-                PortFilter.FACTORY,
-                IntentFilterFilter.FACTORY,
-                ComponentFilter.FACTORY,
-                ProvisionedFilter.FACTORY
-        };
+                    CategoryFilter.FACTORY,
+                    SenderFilter.FACTORY,
+                    SenderPackageFilter.FACTORY,
+                    SenderPermissionFilter.FACTORY,
+                    TargetFilter.FACTORY,
+                    TargetPackageFilter.FACTORY,
+                    TargetPermissionFilter.FACTORY,
+                    PortFilter.FACTORY,
+                    IntentFilterFilter.FACTORY,
+                    ComponentFilter.FACTORY,
+                    ProvisionedFilter.FACTORY,
+                    ExtraKeyValueFilter.FACTORY
+            };
+        } else {
+            factories = new FilterFactory[]{
+                    AndFilter.FACTORY,
+                    OrFilter.FACTORY,
+                    NotFilter.FACTORY,
+
+                    StringFilter.ACTION,
+                    StringFilter.COMPONENT,
+                    StringFilter.COMPONENT_NAME,
+                    StringFilter.COMPONENT_PACKAGE,
+                    StringFilter.DATA,
+                    StringFilter.HOST,
+                    StringFilter.MIME_TYPE,
+                    StringFilter.SCHEME,
+                    StringFilter.PATH,
+                    StringFilter.SSP,
+
+                    CategoryFilter.FACTORY,
+                    SenderFilter.FACTORY,
+                    SenderPackageFilter.FACTORY,
+                    SenderPermissionFilter.FACTORY,
+                    TargetFilter.FACTORY,
+                    TargetPackageFilter.FACTORY,
+                    TargetPermissionFilter.FACTORY,
+                    PortFilter.FACTORY,
+                    IntentFilterFilter.FACTORY,
+                    ComponentFilter.FACTORY,
+                    ProvisionedFilter.FACTORY
+            };
+        }
 
         // load factor ~= .75
         factoryMap = new HashMap<String, FilterFactory>(factories.length * 4 / 3);
