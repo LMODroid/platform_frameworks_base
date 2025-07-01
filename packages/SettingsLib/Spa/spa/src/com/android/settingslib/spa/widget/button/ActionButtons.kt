97154f586f41aae2f8da27b1118f87b8ca5ca5f8
tom hsu <tomhsu@google.com>
2025-07-01 08:42:23 +0000

    [SPA] Fix icon misallign to ActionButton
    Flag: EXEMPT bug fix
    Fix: b/419719457
    Test: Visual
    Change-Id: If6aed1f2e09f00dc9e62ad80cfb0932378238307

--- packages/SettingsLib/Spa/spa/src/com/android/settingslib/spa/widget/button/ActionButtons.kt
+++ packages/SettingsLib/Spa/spa/src/com/android/settingslib/spa/widget/button/ActionButtons.kt
@@ -35,6 +35,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Launch
 import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.DisabledByDefault
 import androidx.compose.material.icons.outlined.WarningAmber
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
@@ -101,13 +102,19 @@ fun ActionButtons(actionButtons: List<ActionButton>) {
 @Composable
 private fun RowScope.ActionButton(actionButton: ActionButton) {
     if (isSpaExpressiveEnabled) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+                .weight(1f)
+        ) {
             IconButton(actionButton)
             Spacer(Modifier.height(SettingsDimension.extraSmall3))
             Text(
                 text = actionButton.text,
-                modifier = Modifier.clearAndSetSemantics {}, // semantics set in IconButton
+                modifier = Modifier
+                    .clearAndSetSemantics {} // semantics set in IconButton
+                    .padding(horizontal = 8.dp),
                 style = MaterialTheme.typography.titleSmallEmphasized,
+                textAlign = TextAlign.Center,
             )
         }
     } else {
@@ -200,6 +207,10 @@ private fun ActionButtonsPreview() {
                 ActionButton(text = "Open", imageVector = Icons.AutoMirrored.Outlined.Launch) {},
                 ActionButton(text = "Uninstall", imageVector = Icons.Outlined.Delete) {},
                 ActionButton(text = "Force stop", imageVector = Icons.Outlined.WarningAmber) {},
+                ActionButton(
+                    text = "long long long long text",
+                    imageVector = Icons.Outlined.DisabledByDefault
+                ) {},
             )
         )
     }
