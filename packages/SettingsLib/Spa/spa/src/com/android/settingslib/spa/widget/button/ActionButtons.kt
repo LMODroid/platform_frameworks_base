33363d1c8d4d6796f2c91190a5c25a39818ee3a1
tom hsu <tomhsu@google.com>
2025-07-09 23:06:46 +0000

    [SPA] Fix GTS failed problem.
     - clearAndSetSemantics cause uiautomater show the UI component to a
       View only. In this case, TextView set this function, so uiautomater
       identify this be a android.view.View but not a TextView.
     - Group Icon and Text to make it be clickable.
    
    Flag: EXEMPT bug fix
    Bug: b/416311339
    Test: Manual test
    Change-Id: I8171a83da8268a254b35f5b9686b9b540bc2eb88

--- packages/SettingsLib/Spa/spa/src/com/android/settingslib/spa/widget/button/ActionButtons.kt
+++ packages/SettingsLib/Spa/spa/src/com/android/settingslib/spa/widget/button/ActionButtons.kt
@@ -17,6 +17,7 @@
 package com.android.settingslib.spa.widget.button
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -103,15 +104,16 @@ fun ActionButtons(actionButtons: List<ActionButton>) {
 private fun RowScope.ActionButton(actionButton: ActionButton) {
     if (isSpaExpressiveEnabled) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
                 .weight(1f)
+                .clickable(onClick = actionButton.onClick)
         ) {
             IconButton(actionButton)
             Spacer(Modifier.height(SettingsDimension.extraSmall3))
             Text(
                 text = actionButton.text,
                 modifier = Modifier
-                    .clearAndSetSemantics {} // semantics set in IconButton
                     .padding(horizontal = 8.dp),
                 style = MaterialTheme.typography.titleSmallEmphasized,
                 textAlign = TextAlign.Center,
@@ -170,7 +172,8 @@ private fun IconButton(actionButton: ActionButton) {
                     IconButtonDefaults.mediumContainerSize(
                         IconButtonDefaults.IconButtonWidthOption.Wide
                     )
-                ),
+                )
+                .clearAndSetSemantics {}, // semantics set in IconButton
         enabled = actionButton.enabled,
         // Because buttons could appear, disappear or change positions, reset the interaction source
         // to prevent highlight the wrong button.
