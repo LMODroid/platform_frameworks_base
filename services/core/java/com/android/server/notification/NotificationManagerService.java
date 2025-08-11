433bda48a2ab03a774bb23b3472f5da3d8b7fbe6
Iavor-Valentin Iftime <valiiftime@google.com>
2025-08-11 08:24:48 -0700

    Revert "Check sound Uri permission when creating a notification channel"
    This reverts commit 9c5a54d27f05111db37cfcb685a39e20aec75c09.
    
    Reason for revert: b/434558786
    
    Bug: 337775777
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:c07c78634b4ef317a286b68dea87a31aec972490
    Merged-In: I54c94df0566bbd2a6fd64f97207c4a4d362a827c
    Change-Id: I54c94df0566bbd2a6fd64f97207c4a4d362a827c

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2516,7 +2516,6 @@ public class NotificationManagerService extends SystemService {
                 mNotificationChannelLogger,
                 mAppOps,
                 mUserProfiles,
-                mUgmInternal,
                 mShowReviewPermissionsNotification,
                 Clock.systemUTC());
         mRankingHelper = new RankingHelper(getContext(), mRankingHandler, mPreferencesHelper,
@@ -6453,7 +6452,13 @@ public class NotificationManagerService extends SystemService {
             final Uri originalSoundUri =
                     (originalChannel != null) ? originalChannel.getSound() : null;
             if (soundUri != null && !Objects.equals(originalSoundUri, soundUri)) {
-                PermissionHelper.grantUriPermission(mUgmInternal, soundUri, sourceUid);
+                Binder.withCleanCallingIdentity(() -> {
+                    mUgmInternal.checkGrantUriPermission(sourceUid, null,
+                            ContentProvider.getUriWithoutUserId(soundUri),
+                            Intent.FLAG_GRANT_READ_URI_PERMISSION,
+                            ContentProvider.getUserIdFromUri(soundUri,
+                            UserHandle.getUserId(sourceUid)));
+                });
             }
         }
 
