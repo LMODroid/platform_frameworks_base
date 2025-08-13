e2fcff4dc74aabf4886c0b267e9da164b683a305
Dhina17 <dhinalogu@gmail.com>
2025-08-13 13:37:54 +0530

    GameSpaceManager: Update foreground app on onTaskMovedToFront() as well
    When we slowly enter the quickstep (recents), sometimes
    the onTaskStackChanged() don't get called so the
    gamespace floating bar stuck at the screen
    until we launch an app.
    
    Change-Id: I8317b78b5222134854f40e02be1425af969085a9

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/GameSpaceManager.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/GameSpaceManager.kt
@@ -51,7 +51,10 @@ class GameSpaceManager @Inject constructor(
 
     private val taskStackChangeListener = object : TaskStackChangeListener {
         override fun onTaskStackChanged() {
-            handler.sendEmptyMessage(MSG_UPDATE_FOREGROUND_APP)
+            updateForegroundApp()
+        }
+        override fun onTaskMovedToFront(taskId: Int) {
+            updateForegroundApp()
         }
     }
 
@@ -80,6 +83,11 @@ class GameSpaceManager @Inject constructor(
                 MSG_DISPATCH_FOREGROUND_APP -> dispatchForegroundApp()
             }
         }
+    }
+
+    private fun updateForegroundApp() {
+        handler.removeMessages(MSG_UPDATE_FOREGROUND_APP)
+        handler.sendEmptyMessageDelayed(MSG_UPDATE_FOREGROUND_APP, 150)
     }
 
     private fun checkForegroundApp() {
