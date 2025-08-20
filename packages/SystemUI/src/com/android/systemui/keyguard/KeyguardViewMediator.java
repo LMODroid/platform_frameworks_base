a21e0e1aa3304bb26ad9829df11e848ee761bdd6
Matt Pietal <mpietal@google.com>
2025-08-20 17:58:26 +0000

    Ensure exit animations are canceled prior to user switch
    On user switch, remove any pending requests and set a flag to indicate
    a cancelation, and verify that flag just prior to handling the
    exit animation callback from window manager.
    
    Bug: 407562568
    Test: atest KeyguardViewMediatorTest
    Flag: EXEMPT bugfix
    (cherry picked from commit f17b66e731224605373efc27c448a048e954d663)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:5adb31670830e04cff3b49960875a551c1cccf0a
    Merged-In: Ib262129422e6198f019482547a6b129cc60f0865
    Change-Id: Ib262129422e6198f019482547a6b129cc60f0865

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2422,6 +2422,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
                 mHandler.removeCallbacksAndMessages(mDismissToken);
                 mHandler.removeMessages(DISMISS);
                 mHandler.removeMessages(HIDE);
+                mHandler.removeMessages(START_KEYGUARD_EXIT_ANIM);
                 notifyTrustedChangedLocked(mUpdateMonitor.getUserHasTrust(newUserId));
                 resetKeyguardDonePendingLocked();
                 adjustStatusBarLocked();
@@ -3222,6 +3223,12 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
         }
     }
 
+    // Allows the runnable to be controlled for tests by overriding this method
+    @VisibleForTesting
+    void postAfterTraversal(Runnable runnable) {
+        DejankUtils.postAfterTraversal(runnable);
+    }
+
     /**
      * Called when we're done running the keyguard exit animation, we should now end up unlocked.
      *
@@ -3253,13 +3260,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
         InteractionJankMonitor.getInstance().end(CUJ_LOCKSCREEN_UNLOCK_ANIMATION);
 
         // Post layout changes to the next frame, so we don't hang at the end of the animation.
-        DejankUtils.postAfterTraversal(() -> {
-            if (mIsKeyguardExitAnimationCanceled) {
-                Log.d(TAG, "Ignoring dejank exitKeyguardAndFinishSurfaceBehindRemoteAnimation. "
-                        + "mIsKeyguardExitAnimationCanceled==true");
-                return;
-            }
-
+        postAfterTraversal(() -> {
             if (!mPM.isInteractive() && !mPendingLock) {
                 Log.e(TAG, "exitKeyguardAndFinishSurfaceBehindRemoteAnimation#postAfterTraversal:"
                         + " mPM.isInteractive()=" + mPM.isInteractive()
@@ -3271,6 +3272,13 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
                 // Ensure WM is notified that we made a decision to show
                 setShowingLocked(true /* showing */, true /* force */);
 
+                return;
+            }
+            if (mIsKeyguardExitAnimationCanceled) {
+                Log.d(TAG, "Ignoring exitKeyguardAndFinishSurfaceBehindRemoteAnimation. "
+                        + "mIsKeyguardExitAnimationCanceled==true");
+                finishSurfaceBehindRemoteAnimation(true /* showKeyguard */);
+                setShowingLocked(true /* showing */, true /* force */);
                 return;
             }
 
