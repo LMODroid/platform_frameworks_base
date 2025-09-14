26f814840bfe7196eaa0d1696328f0cf276c1c96
Pranav Vashi <neobuddy89@gmail.com>
2025-09-14 18:30:48 +0530

    BrightnessSlider: Extend haptics for auto brightness button
    Change-Id: I10aed7c17415e0fc833ea97cb47c4bff91945803
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/brightness/ui/compose/BrightnessSlider.kt
+++ packages/SystemUI/src/com/android/systemui/brightness/ui/compose/BrightnessSlider.kt
@@ -21,6 +21,7 @@ import android.content.Context
 import android.database.ContentObserver
 import android.provider.Settings
 import android.os.UserHandle
+import android.view.HapticFeedbackConstants
 import android.view.MotionEvent
 import androidx.annotation.VisibleForTesting
 import androidx.compose.animation.animateColorAsState
@@ -83,6 +84,7 @@ import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -418,6 +420,7 @@ private fun drawAutoBrightnessButton(
     autoMode: Boolean,
     onIconClick: suspend () -> Unit,
 ) {
+    val view = LocalView.current
     val coroutineScope = rememberCoroutineScope()
     val backgroundColor by animateColorAsState(
         targetValue = if (autoMode) {
@@ -438,9 +441,17 @@ private fun drawAutoBrightnessButton(
     } else {
         R.drawable.ic_qs_brightness_auto_off
     }
+    val hapticConstant = if (autoMode) {
+        HapticFeedbackConstants.TOGGLE_OFF
+    } else {
+        HapticFeedbackConstants.TOGGLE_ON
+    }
 
     IconButton(
-        onClick = { coroutineScope.launch { onIconClick() } },
+        onClick = {
+            view.performHapticFeedback(hapticConstant)
+            coroutineScope.launch { onIconClick() }
+        },
         modifier = Modifier
             .size(52.dp)
             .clip(CircleShape)
