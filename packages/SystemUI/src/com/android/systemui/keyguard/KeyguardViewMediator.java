5c82534ce5df48dc5adc3c3c0e71dfc7e51667c1
beverlyt <beverlyt@google.com>
2025-09-15 15:57:25 +0000

    RESTRICT AUTOMERGE Check the strongAuthTracker for lockdown instead of LockPatternUtils
    LockPatternUtils and the StrongAuthTracker check for
    lockdown may be out of sync, so always check the
    KeyguardUpdateMonitor's strong auth tracker for
    lockdown.
    
    Test: atest KeyguardViewMediatorTest
    Test: pin an app; lock down device;
    observe keyguard always shows (repeat 10x times)
    Bug: 438973280
    Flag: EXEMPT CVE_FIX
    
    (cherry picked from commit 5c25e76eb886ad3508103b486b146b6920c8c651)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:52a6d6dfda83c584c99a39321f85bb235c6667fc
    Merged-In: I076f5a8cacd14fc8ab8893aedf98ff831a9c195f
    Change-Id: I076f5a8cacd14fc8ab8893aedf98ff831a9c195f

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -780,7 +780,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
 
         @Override
         public void onStrongAuthStateChanged(int userId) {
-            if (mLockPatternUtils.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
+            if (mUpdateMonitor.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
                 doKeyguardLocked(null);
             }
         }
@@ -1949,7 +1949,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
             mExternallyEnabled = enabled;
 
             if (!enabled && mShowing) {
-                if (mLockPatternUtils.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
+                if (mUpdateMonitor.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
                     Log.d(TAG, "keyguardEnabled(false) overridden by user lockdown");
                     return;
                 }
@@ -2186,7 +2186,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
 
         // if another app is disabling us, don't show
         if (!mExternallyEnabled
-                && !mLockPatternUtils.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
+                && !mUpdateMonitor.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
             if (DEBUG) Log.d(TAG, "doKeyguard: not showing because externally disabled");
             notifyLockNowCallback();
             mNeedToReshowWhenReenabled = true;
