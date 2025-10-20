c2caec7c50349e49dd4b497a05bd2f8f04b282cb
Achim Thesmann <achim@google.com>
2025-10-20 14:09:41 -0700

    Revert "RESTRICT AUTOMERGE Ignore pinned Windows"
    This reverts commit 85f96289058efd23af98480e8cc74cc073662676.
    
    Reason for revert: app compatibility regression
    
    Bug: 406880479
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:1ee36749484bf96fd866081c24f7bca8fd47fdd7
    Merged-In: I7f1239055d24718529afdd34e7b44497f7051518
    Change-Id: I7f1239055d24718529afdd34e7b44497f7051518

--- services/core/java/com/android/server/wm/BackgroundLaunchProcessController.java
+++ services/core/java/com/android/server/wm/BackgroundLaunchProcessController.java
@@ -100,8 +100,7 @@ class BackgroundLaunchProcessController {
     BalVerdict areBackgroundActivityStartsAllowed(
             int pid, int uid, String packageName,
             int appSwitchState, boolean isCheckingForFgsStart,
-            boolean hasActivityInVisibleTask, boolean inPinnedWindow,
-            boolean hasBackgroundActivityStartPrivileges,
+            boolean hasActivityInVisibleTask, boolean hasBackgroundActivityStartPrivileges,
             long lastStopAppSwitchesTime, long lastActivityLaunchTime,
             long lastActivityFinishTime) {
         // Allow if the proc is instrumenting with background activity starts privs.
@@ -124,11 +123,9 @@ class BackgroundLaunchProcessController {
             return new BalVerdict(BAL_ALLOW_VISIBLE_WINDOW, /*background*/ false,
                     "process bound by foreground uid");
         }
-        // Allow if the caller has an activity in any foreground task, unless it's a pinned window
-        // and not a foreground service start.
-        if ((isCheckingForFgsStart || !inPinnedWindow)
-                && hasActivityInVisibleTask && appSwitchState != APP_SWITCH_DISALLOW) {
-            return new BalVerdict(BAL_ALLOW_FOREGROUND, /*background*/
+        // Allow if the caller has an activity in any foreground task.
+        if (hasActivityInVisibleTask && appSwitchState != APP_SWITCH_DISALLOW) {
+            return new BalVerdict(BAL_ALLOW_FOREGROUND, /*background*/ false,
                     "process has activity in foreground task");
         }
 
