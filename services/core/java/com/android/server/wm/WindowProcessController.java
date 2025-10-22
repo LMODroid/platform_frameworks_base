1eef5c796ea16f8187f4781e9410c34535d618c3
Achim Thesmann <achim@google.com>
2025-10-22 17:05:10 +0000

    RESTRICT AUTOMERGE Ignore pinned Windows (Consolidated fix)
    ag/32704936
    ag/35782210
    ag/36195227
    ag/36359572
    
    Bug: 439984935
    Test: atest BackgroundActivityLaunchTest
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:d7e9d1e8b8fd3202dfc8a91772ae7c7d1e88e2c8
    Merged-In: I9ce93366a0876c7bc594ef818c8598342d70513d
    Change-Id: I9ce93366a0876c7bc594ef818c8598342d70513d

--- services/core/java/com/android/server/wm/WindowProcessController.java
+++ services/core/java/com/android/server/wm/WindowProcessController.java
@@ -700,7 +700,8 @@ public class WindowProcessController extends ConfigurationContainer<Configuratio
             int appSwitchState, boolean isCheckingForFgsStart) {
         return mBgLaunchController.areBackgroundActivityStartsAllowed(mPid, mUid,
                 mInfo.packageName, appSwitchState, isCheckingForFgsStart,
-                hasActivityInVisibleTask(), mInstrumentingWithBackgroundActivityStartPrivileges,
+                hasActivityInVisibleTask(), inPinnedWindowingMode(),
+                mInstrumentingWithBackgroundActivityStartPrivileges,
                 mAtm.getLastStopAppSwitchesTime(),
                 mLastActivityLaunchTime, mLastActivityFinishTime);
     }
@@ -867,6 +868,21 @@ public class WindowProcessController extends ConfigurationContainer<Configuratio
     @HotPath(caller = HotPath.LRU_UPDATE)
     public boolean hasActivitiesOrRecentTasks() {
         return mHasActivities || mHasRecentTasks;
+    }
+
+    /**
+     * Check if any Activity is visible (or visibility requested) and not pinned.
+     */
+    boolean hasVisibleNotPinnedActivity() {
+        if (!hasVisibleActivities()) return false;
+        for (int i = mActivities.size() - 1; i >= 0; --i) {
+            final ActivityRecord activityRecord = mActivities.get(i);
+            if ((activityRecord.isVisible() || activityRecord.isVisibleRequested())
+                    && !activityRecord.inPinnedWindowingMode()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Nullable
