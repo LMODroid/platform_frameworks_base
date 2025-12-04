634ed9704ef11ad094fa916670387e224f38bf9c
Bruno Martins <bgcngm@gmail.com>
2025-12-04 10:50:59 +0000

    fixup! SystemUI: Bring back good ol' circle battery style
    Change-Id: I9fa801cc4a03c977e611a663a6f443c9430b6815

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -661,7 +661,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
 
         mBatteryStateAlert = isAlert;
         if (!newStatusBarIcons()) {
-            mDrawable.setAlertEnabled(isAlert);
+            mAccessorizedDrawable.setAlertEnabled(isAlert);
         }
     }
 
