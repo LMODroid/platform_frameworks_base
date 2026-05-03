OrionOS-Project/frameworks_base

Add level to flashlight tile
Flag: com.android.systemui.flashlight_strength
Fixes: 399470157
Fixes: 399465570
Fixes: 399492164
Fixes: 399468001
Test: atest FlashlightTileWithLevelTest
Test: atest FlashlightTileMapperTest FlashlightTileDataInteractorTest FlashlightTileUserActionInteractorTest
Change-Id: If2e96ca6ed1d841210f307fbe1b8f646b0292816

packages/SystemUI/src/com/android/systemui/statusbar/policy/PolicyModule.kt
@@ -22,6 +22,8 @@ import android.os.UserManager.DISALLOW_MICROPHONE_TOGGLE
 import android.os.UserManager.DISALLOW_SHARE_LOCATION
 import com.android.systemui.Flags
 import com.android.systemui.flashlight.FlashlightModule
+import com.android.systemui.flashlight.flags.FlashlightStrength
+import com.android.systemui.flashlight.shared.model.FlashlightModel
 import com.android.systemui.qs.QsEventLogger
 import com.android.systemui.qs.pipeline.shared.TileSpec
 import com.android.systemui.qs.shared.model.TileCategory
@@ -29,6 +31,7 @@ import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.AlarmTile
 import com.android.systemui.qs.tiles.CameraToggleTile
 import com.android.systemui.qs.tiles.FlashlightTile
+import com.android.systemui.qs.tiles.FlashlightTileWithLevel
 import com.android.systemui.qs.tiles.LocationTile
 import com.android.systemui.qs.tiles.MicrophoneToggleTile
 import com.android.systemui.qs.tiles.ModesDndTile
@@ -48,8 +51,7 @@ import com.android.systemui.qs.tiles.impl.alarm.domain.model.AlarmTileModel
 import com.android.systemui.qs.tiles.impl.alarm.ui.mapper.AlarmTileMapper
 import com.android.systemui.qs.tiles.impl.flashlight.domain.interactor.FlashlightTileDataInteractor
 import com.android.systemui.qs.tiles.impl.flashlight.domain.interactor.FlashlightTileUserActionInteractor
-import com.android.systemui.qs.tiles.impl.flashlight.domain.model.FlashlightTileModel
-import com.android.systemui.qs.tiles.impl.flashlight.ui.mapper.FlashlightMapper
+import com.android.systemui.qs.tiles.impl.flashlight.ui.mapper.FlashlightTileMapper
 import com.android.systemui.qs.tiles.impl.location.domain.interactor.LocationTileDataInteractor
 import com.android.systemui.qs.tiles.impl.location.domain.interactor.LocationTileUserActionInteractor
 import com.android.systemui.qs.tiles.impl.location.domain.model.LocationTileModel
@@ -81,6 +83,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
 import dagger.multibindings.StringKey
+import javax.inject.Provider
 
 @Module(includes = [FlashlightModule::class])
 interface PolicyModule {
@@ -94,7 +97,7 @@ interface PolicyModule {
     @Binds
     @IntoMap
     @StringKey(FLASHLIGHT_TILE_SPEC)
-    fun provideAirplaneModeAvailabilityInteractor(
+    fun provideFlashlightAvailabilityInteractor(
         impl: FlashlightTileDataInteractor
     ): QSTileAvailabilityInteractor
 
@@ -170,8 +173,8 @@ interface PolicyModule {
         @IntoMap
         @StringKey(FLASHLIGHT_TILE_SPEC)
         fun provideFlashlightTileViewModel(
-            factory: QSTileViewModelFactory.Static<FlashlightTileModel>,
-            mapper: FlashlightMapper,
+            factory: QSTileViewModelFactory.Static<FlashlightModel>,
+            mapper: FlashlightTileMapper,
             stateInteractor: FlashlightTileDataInteractor,
             userActionInteractor: FlashlightTileUserActionInteractor,
         ): QSTileViewModel =
@@ -470,13 +473,20 @@ interface PolicyModule {
                 stateInteractor,
                 mapper,
             )
-    }
 
-    /** Inject FlashlightTile into tileMap in QSModule */
-    @Binds
-    @IntoMap
-    @StringKey(FlashlightTile.TILE_SPEC)
-    fun bindFlashlightTile(flashlightTile: FlashlightTile): QSTileImpl<*>
+        /**
+         * Inject FlashlightTile or FlashlightTileWithLevel into tileMap in QSModule based on flag
+         */
+        @Provides
+        @IntoMap
+        @StringKey(FlashlightTile.TILE_SPEC)
+        fun provideBinaryOrLevelOldFlashlightTile(
+            binaryTile: Provider<FlashlightTile>,
+            levelTile: Provider<FlashlightTileWithLevel>,
+        ): QSTileImpl<*> {
+            return if (FlashlightStrength.isEnabled) levelTile.get() else binaryTile.get()
+        }
+    }
 
     /** Inject LocationTile into tileMap in QSModule */
     @Binds

LMODroid/platform_frameworks_base

Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/policy/PolicyModule.kt
@@ -22,6 +22,8 @@ import android.os.UserManager.DISALLOW_MICROPHONE_TOGGLE
 import android.os.UserManager.DISALLOW_SHARE_LOCATION
 import com.android.systemui.Flags
 import com.android.systemui.flashlight.FlashlightModule
+import com.android.systemui.flashlight.flags.FlashlightStrength
+import com.android.systemui.flashlight.shared.model.FlashlightModel
 import com.android.systemui.modes.shared.ModesUi
 import com.android.systemui.qs.QsEventLogger
 import com.android.systemui.qs.pipeline.shared.TileSpec
@@ -31,6 +33,8 @@ import com.android.systemui.qs.tiles.AlarmTile
 import com.android.systemui.qs.tiles.CameraToggleTile
 import com.android.systemui.qs.tiles.DndTile
 import com.android.systemui.qs.tiles.FlashlightStrengthTile
+import com.android.systemui.qs.tiles.FlashlightTile
+import com.android.systemui.qs.tiles.FlashlightTileWithLevel
 import com.android.systemui.qs.tiles.LocationTile
 import com.android.systemui.qs.tiles.MicrophoneToggleTile
 import com.android.systemui.qs.tiles.ModesDndTile
@@ -50,8 +54,7 @@ import com.android.systemui.qs.tiles.impl.alarm.domain.model.AlarmTileModel
 import com.android.systemui.qs.tiles.impl.alarm.ui.mapper.AlarmTileMapper
 import com.android.systemui.qs.tiles.impl.flashlight.domain.interactor.FlashlightTileDataInteractor
 import com.android.systemui.qs.tiles.impl.flashlight.domain.interactor.FlashlightTileUserActionInteractor
-import com.android.systemui.qs.tiles.impl.flashlight.domain.model.FlashlightTileModel
-import com.android.systemui.qs.tiles.impl.flashlight.ui.mapper.FlashlightMapper
+import com.android.systemui.qs.tiles.impl.flashlight.ui.mapper.FlashlightTileMapper
 import com.android.systemui.qs.tiles.impl.location.domain.interactor.LocationTileDataInteractor
 import com.android.systemui.qs.tiles.impl.location.domain.interactor.LocationTileUserActionInteractor
 import com.android.systemui.qs.tiles.impl.location.domain.model.LocationTileModel
@@ -97,7 +100,7 @@ interface PolicyModule {
     @Binds
     @IntoMap
     @StringKey(FLASHLIGHT_TILE_SPEC)
-    fun provideAirplaneModeAvailabilityInteractor(
+    fun provideFlashlightAvailabilityInteractor(
         impl: FlashlightTileDataInteractor
     ): QSTileAvailabilityInteractor
 
@@ -180,8 +183,8 @@ interface PolicyModule {
         @IntoMap
         @StringKey(FLASHLIGHT_TILE_SPEC)
         fun provideFlashlightTileViewModel(
-            factory: QSTileViewModelFactory.Static<FlashlightTileModel>,
-            mapper: FlashlightMapper,
+            factory: QSTileViewModelFactory.Static<FlashlightModel>,
+            mapper: FlashlightTileMapper,
             stateInteractor: FlashlightTileDataInteractor,
             userActionInteractor: FlashlightTileUserActionInteractor,
         ): QSTileViewModel =
@@ -493,13 +496,20 @@ interface PolicyModule {
                 stateInteractor,
                 mapper,
             )
-    }
 
-    /** Inject FlashlightStrengthTile into tileMap in QSModule */
-    @Binds
-    @IntoMap
-    @StringKey(FlashlightStrengthTile.TILE_SPEC)
-    fun bindFlashlightTile(flashlightStrengthTile: FlashlightStrengthTile): QSTileImpl<*>
+        /**
+         * Inject FlashlightTile or FlashlightTileWithLevel into tileMap in QSModule based on flag
+         */
+        @Provides
+        @IntoMap
+        @StringKey(FlashlightTile.TILE_SPEC)
+        fun provideBinaryOrLevelOldFlashlightTile(
+            binaryTile: Provider<FlashlightStrengthTile>,
+            levelTile: Provider<FlashlightTileWithLevel>,
+        ): QSTileImpl<*> {
+            return if (FlashlightStrength.isEnabled) levelTile.get() else binaryTile.get()
+        }
+    }
 
     /** Inject LocationTile into tileMap in QSModule */
     @Binds