e3479dd488ff92b3afa9c260c0bd8f9964cefdd5
SagarMakhar <makharsagar@gmail.com>
2026-06-05 00:15:15 +0530

    Add missing QSTileConfig for NfcTile
    Change-Id: Ie3be19004f0aa88d3de33c6f38c1c184cafc5f6e

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/ConnectivityModule.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/ConnectivityModule.kt
@@ -290,5 +290,20 @@ interface ConnectivityModule {
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.CONNECTIVITY,
             )
+
+        @Provides
+        @IntoMap
+        @StringKey(NfcTile.TILE_SPEC)
+        fun provideNfcTileConfig(uiEventLogger: QsEventLogger): QSTileConfig =
+            QSTileConfig(
+                tileSpec = TileSpec.create(NfcTile.TILE_SPEC),
+                uiConfig =
+                    QSTileUIConfig.Resource(
+                        iconRes = R.drawable.ic_qs_nfc,
+                        labelRes = R.string.quick_settings_nfc_label
+                    ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.CONNECTIVITY,
+            )
     }
 }
