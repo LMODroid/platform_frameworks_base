ProjectNirwana/frameworks_base

Remove unused old transition types
Bug: 365884835
Flag: EXEMPT remove dead code
Test: WindowContainerTests

Change-Id: Id4c44b1abfdbec9ec21ba5db94f96f2f38561a24

services/core/java/com/android/server/wm/DisplayContent.java
@@ -4640,7 +4640,7 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
                 t.show(mSurface);
                 if (DEBUG_IME_VISIBILITY) {
                     EventLog.writeEvent(IMF_SHOW_IME_SCREENSHOT, mImeTarget.toString(),
-                            dc.mInputMethodWindow.mTransitFlags, mSurfacePosition.toString());
+                            0 /* unused transition flags */, mSurfacePosition.toString());
                 }
             } else if (!isValid) {
                 removeSurface(t);

LMODroid/platform_frameworks_base

services/core/java/com/android/server/wm/DisplayContent.java
@@ -4630,7 +4630,7 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
                 t.show(mImeSurface);
                 if (DEBUG_IME_VISIBILITY) {
                     EventLog.writeEvent(IMF_SHOW_IME_SCREENSHOT, mImeTarget.toString(),
-                            dc.mInputMethodWindow.mTransitFlags, mImeSurfacePosition.toString());
+                            0 /* unused transition flags */, mImeSurfacePosition.toString());
                 }
             } else if (!isValidSnapshot) {
                 removeImeSurface(t);